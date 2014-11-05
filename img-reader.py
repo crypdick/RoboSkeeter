29c7830a62f88fde68d3506c26c79f0ee51a4118
isomerase <richard.decal@gmail.com>
2014-11-04 16:37:53 -0800

    solved img-reader

--- img-reader.py
+++ img-reader.py
@@ -3,33 +3,22 @@
 Created on Mon Nov  3 15:25:28 2014
 
 @author: richard
+
+given an (x,y) return intensity value
 """
 
-#v1
-#from PIL import Image
-#import numpy as np
-#im=Image.open("vid1-000.png")
-#pixels=np.asarray(im.getdata())
-#npixels,bpp=pixels.shape
-#
-#with open('out.txt', 'w') as f:
-#    f.write(pixels)
-
-
-## v2
-#import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
-#import numpy as np
-#
-#imgasarray=mpimg.imread('vid1-000.png')
-#imgasarray.tofile('arrayfile.txt')
-##np.savetxt('test.txt', imgasarray)
 
 from PIL import Image
-from glob import glob
-import numpy as np
-img =Image.open("vid1-000.png").convert('L')
-img.getpixel((60,0))
+#from glob import glob
+#import numpy as np
 
-for filename in glob('./vid1/*'):
-    print filename
+def pixel_intensity(coord, imgdir): #coord is (x,y)
+    img =Image.open(imgdir).convert('L')
+    return img.getpixel(coord)
+
+#img =Image.open("vid1-000.png").convert('L')
+#files = []
+#for filename in glob('./vid1/*'):
+#    files.append(filename)
+#
+#filesenum =list(enumerate(files))
