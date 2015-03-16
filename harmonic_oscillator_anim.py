3c38348ff24a002417b49f9f032dc50d185db053
rdecal <decal@uw.edu>
2015-03-16 13:32:54 -0700

    various variable renaming, chopping code

--- harmonic_oscillator_anim.py
+++ harmonic_oscillator_anim.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
+TODO: automatically save this video
+
 stolen from https://github.com/stefanv/vibrations
 
-Created on Mon Mar 16 13:10:09 2015
+Created on Mon Mar 16 11:40:53 2015
 @author: Richard Decal, decal@uw.edu
 https://staff.washington.edu/decal/
 https://github.com/isomerase/
@@ -20,82 +22,89 @@ import matplotlib.animation as animation
 from matplotlib.patches import Circle
 
 
-# Workaround to matplotlib bug in FunctionAnimator that won't allow us
-# to specify ``itertools.count(step=delta)`` as the frames argument.
-# Also see ``set_time`` below.
-delta = 0.02
+# Workaround to matplotlib bug in Functionheadingnimator that won't allow us
+# to specify ``itertools.count(step=dt)`` as the frames argument.
+# headinglso see ``set_time`` below.
+dt = 0.02
+
+
+#m is the mass, y the damping from the dash pot, k the restoring force of the spring, F the driving force and w the frequency.
 
 
 class FreeUndamped(object):
-    def __init__(self, k, m, u0, u0_prime):
-        self.w0 = np.sqrt(k / m)
-        self.B = u0
-        self.A = u0_prime / self.w0
+    def __init__(self, k, m, x0, x0_prime):
+        self.ang_freq0 = np.sqrt(k / m) #angular frequency
+        self.start_pos = x0
+        self.heading = x0_prime / self.ang_freq0
 
     def __call__(self, t):
-        A, B, w0 = self.A, self.B, self.w0
-        return A * np.sin(w0 * t) + B * np.cos(w0 * t)
+        heading, start_pos, ang_freq0 = self.heading, self.start_pos, self.ang_freq0
+        return heading * np.sin(ang_freq0 * t) + start_pos * np.cos(ang_freq0 * t)
 
 
-class Spring(object):
+class Agent(object):
     N = 100
     _hist_length = 100
 
-    _spring_coords = np.zeros(N)
-    _spring_coords[30:70] = 0.05 * (-1) ** np.arange(40)
+    _agent_coords = np.zeros(N)
+    _agent_coords[30:70] = 0.05 * (-1) ** np.arange(40)
 
-    def __init__(self, axis, axis_history, k, m, gamma, F, u0, u0_prime):
-        if gamma == 0 or F == 0:
-            self._method = FreeUndamped(k, m, u0, u0_prime)
+    def __init__(self, axis, axis_history, k, m, gamma, F, x0, x0_prime):
+        if gamma == 0 or F == 0: #gamma is damping
+            self._method = FreeUndamped(k, m, x0, x0_prime)
         else:
-            raise NotImplementedError()
-
+            raise NotImplementedError() #damping is not implemented??
+        
+        #drawing the coords of all the moving bits
         self._t = 0
-        self._anchor = axis.vlines([0], -0.1, 0.1, linewidth=5, color='black')
-        self._pot = Circle((self.u, 0), 0.05, color='black')
-        self._spring, = axis.plot(*self._spring_xy(), color='black')
-
+#        self._anchor = axis.vlines([0], -0.1, 0.1, linewidth=5, color='black') #draw the spring
+        self._pot = Circle((self.x, 0), 0.05, color='black') #draw the mass
+#        self._agent, = axis.plot(*self._agent_xy_positions(), color='black')  #draw spring thingie
+#
         axis.vlines([1], -0.1, -0.2)
-        axis.text(1, -0.25, '$u = 0$', horizontalalignment='center')
-
-        self._ax = axis
+        axis.text(1, -0.25, '$x = 0$', horizontalalignment='center')
+#
+#        self._ax = axis
         axis.add_patch(self._pot)
         axis.set_xlim([0, 2])
         axis.set_ylim([-0.3, 0.2])
         axis.set_axis_off()
         axis.set_aspect('equal')
 
-        self._history = [self.u - 1] * self._hist_length
+        # start of second plot
+        self._history = [self.x - 1] * self._hist_length
         self._history_plot, = axis_history.plot(np.arange(self._hist_length) *
-                                                delta, self._history)
-        axis_history.annotate('Now',
-                              (self._hist_length * delta, 1.5),
-                              (self._hist_length * delta, 1.8),
-                              arrowprops=dict(arrowstyle='->'),
-                              horizontalalignment='center')
+                                                dt, self._history)
+#        axis_history.annotate('Now', #arrow schwag
+#        
+##                              (self._hist_length * dt, 1.5),
+##                              (self._hist_length * dt, 1.8),
+#                              arrowprops=dict(arrowstyle='->'),
+#                              horizontalalignment='center')
         axis_history.set_ylim(-2, 1.5)
         axis_history.set_xticks([])
         axis_history.set_xlabel(r'$\mathrm{Time}$')
-        axis_history.set_ylabel(r'$\mathrm{Position,\, u}$')
+        axis_history.set_ylabel(r'$\mathrm{Position,\, x}$')
 
-    def _spring_xy(self):
-        return np.linspace(0, self.u, self.N), self._spring_coords
+    def _agent_xy_positions(self):
+        return np.linspace(0, self.x, self.N), self._agent_coords
 
-    def set_time(self, t):
-        self._t = t * delta
+    def set_time(self, t):  #TODO wtf
+        self._t = t * dt
         self.update()
 
-    @property
-    def u(self):
+    @property  #  TODO wtf?
+    def x(self):  #  TODO wtf?
         return 1 + self._method(self._t)
 
     def update(self):
-        self._pot.center = (self.u, 0)
+        self._pot.center = (self.x, 0)
+        
+        #these are the handles for plotting
+        x_positions, y_positions = self._agent_xy_positions() #y_positions unused
+#        self._agent.set_xdata(x_positions)
 
-        x, y = self._spring_xy()
-        self._spring.set_xdata(x)
-
-        self._history.append(self.u - 1)
+        self._history.append(self.x - 1)
         self._history = self._history[-self._hist_length:]
 
         self._history_plot.set_ydata(self._history)
@@ -108,26 +117,26 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output',
                         help='Filename for output video.  Requires ffmpeg.')
     parser.add_argument('-m', '--mass', type=float, default=0.5)
-    parser.add_argument('-y', '--damping', type=float, default=0)
+    parser.add_argument('-gamma', '--damping', type=float, default=0)
     parser.add_argument('-k', '--spring', type=float, default=1)
     parser.add_argument('-F', '--force', type=float, default=0)
-    parser.add_argument('--u0', type=float, default=0)
-    parser.add_argument('--u0_prime', type=float, default=0.5)
+    parser.add_argument('--x0', type=float, default=0)
+    parser.add_argument('--x0_prime', type=float, default=0.5)
     args = parser.parse_args()
 
-    m, gamma, k, F, u0, u0_prime = (getattr(args, name) for name in
+    m, gamma, k, F, x0, x0_prime = (getattr(args, name) for name in
                                     ('mass', 'damping', 'spring', 'force',
-                                     'u0', 'u0_prime'))
+                                     'x0', 'x0_prime'))
 
-    f, (ax0, ax1) = plt.subplots(2, 1)
-    s = Spring(axis=ax0, axis_history=ax1,
-               k=k, m=m, gamma=gamma, F=F, u0=u0, u0_prime=u0_prime)
+    fig, (ax0, ax1) = plt.subplots(2, 1) #ax0 is handle for pot, ax1 is handle for the lower plot
+    inst_agent = Agent(axis=ax0, axis_history=ax1,
+               k=k, m=m, gamma=gamma, F=F, x0=x0, x0_prime=x0_prime)
 
-    anim = animation.FuncAnimation(f, s.set_time, interval=delta * 1000,
-                                  save_count=400)
+    anim = animation.FuncAnimation(fig, inst_agent.set_time, interval=dt * 10, #intervial draws new frame every "interval" ms and redraws image
+                                  )#save_count=400)
 
     if args.output:
         print "Saving video output to %s (this may take a while)." % args.output
-        anim.save(args.output, fps=25)
+        anim.save(args.output, fps=40)
 
     plt.show()
