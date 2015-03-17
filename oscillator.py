177ae6786cebaab20d744be7f3937d3aa97ae9d7
rdecal <decal@uw.edu>
2015-03-17 14:43:44 -0700

    beefed up comments; ran code through PEP8 to reformat code to fit Pythonic style conventions

--- oscillator.py
+++ oscillator.py
@@ -4,40 +4,45 @@ Damped harmonic oscillator agent model
 
 Motion for the damped oscillator is described as:
 
-m*d^2x/dt^2 + 2*zeta*w0*(dx/dt) + w0^2 * x = 0
+m*(d^2x/dt^2) + 2*zeta*w0*(dx/dt) + w0^2*x = 0
 
-where x is the positon of the oscillator in one dimension, w0 i the frequency, 
-zeta is the damping coefficient, and m is mass. 
+where x is the positon of the oscillator in one dimension, w0 i the frequency,
+zeta is the damping coefficient, and m is mass.
 
 w0 is the natural frequency of the harmonic oscillator, which is:
 w0 = sqrt(k/m)
 
-A second order ODE (odeint) is used to solve for the position of the mass, 
+A second order ODE (odeint) is used to solve for the position of the mass,
 presently in 1D.
 
-End goal: run in 2d, add driving force and bias.
-
-Code modified from http://nbviewer.ipython.org/github/kialio/scientific-python-lectures/blob/master/Lecture-3-Scipy.ipynb
-Variable names were changed to reflect terms in the original equations.
+End goal: run in 2d, add driving forces (random and temperature-stimulus bias).
 
 Created on Mon Mar 16 16:22:47 2015
 @authors: Richard Decal, decal@uw.edu
         Sharri Zamore
-        
+
 https://staff.washington.edu/decal/
 https://github.com/isomerase/
+
+Code forked from Lecture-3-Scipy.ipynb from the SciPy lectures:
+https://github.com/jrjohansson/scientific-python-lectures
+authored by J.R. Johansson (robert@riken.jp) http://dml.riken.jp/~rob/
+
+Variable names have been changed to reflect terms in the original equations.
 """
 from scipy.integrate import odeint, ode
 from matplotlib import pyplot as plt
 import numpy as np
 
-#TODO: make user-input
-m = 1   #mass
-k = .02   #spring constant
 
-#TODO: make Bias a class? Talk to Rich P about a smart way to make this object oriented.
+# TODO: make user-input
+m = 1.0   # mass of agent
+k = 0.02   # spring constant
 
-#TODO: change output from y to another name, as we will need y for 2D solutions
+# TODO: make Bias a class? Talk to Rich P about a smart way to make this object oriented.
+
+# TODO: rename y, we will need that when we expand to 2D
+
 
 def dy(y, t, zeta, w0):
     """
@@ -53,39 +58,36 @@ def dy(y, t, zeta, w0):
     dxdot = (-2 * zeta * w0 * dxdt - w0**2 * x) / m
 
     return [dx, dxdot]
-    
-# initial state: 
+
+# initial state of the spring
 x0 = [1.0, 0.0]
 #y0 = [1.0 0.0]
 #unclear of how this is determined.
 
 # time coodinate to solve the ODE for
-dt = 1 #timestep
+dt = 1  # timestep width
 runtime = 1000
-num_dt = runtime/dt
+num_dt = runtime/dt  # number of timebins
 
-t = np.linspace(0, num_dt, runtime)
+t = np.linspace(0, num_dt, runtime)  # TODO: shouldn't this be 0, runtime, num_dt?
 w0 = np.sqrt(k/m)
-zeta = 0;   #maybe rename this? I don't like using the same name for the ode input and the function 
+zeta = 0   # maybe rename this? I don't like using the same name for the ode input and the function
 
-#TODO: change the ode such that if zeta is an n x 1 array, it produces n solution arrays
+# TODO: change the ode such that if zeta is an n x 1 array, it produces n solution arrays
 
 # solve the ODE problem for three different values of the damping ratio
-z1 = odeint(dy, x0, t, args=(zeta, w0)) # undamped
+z1 = odeint(dy, x0, t, args=(zeta, w0))  # undamped
 
 #let's leave the other variations out of this for now
-#y2 = odeint(dy, y0, t, args=(0.2, w0)) # under damped
-#y3 = odeint(dy, y0, t, args=(1.0, w0)) # critial damping
-#y4 = odeint(dy, y0, t, args=(5.0, w0)) # over damped
-
-
+#y2 = odeint(dy, y0, t, args=(0.2, w0))  # under damped
+#y3 = odeint(dy, y0, t, args=(1.0, w0))  # critial damping
+#y4 = odeint(dy, y0, t, args=(5.0, w0))  # over damped
 
 fig, ax = plt.subplots()
-ax.plot(t, z1[:,0], 'k', label="undamped", linewidth=0.25)
+ax.plot(t, z1[:, 0], 'k', label="undamped", linewidth=0.25)
 #plt.ylim(-2,2)
 
 #ax.plot(t, y2[:,0], 'r', label="under damped")
 #ax.plot(t, y3[:,0], 'b', label=r"critical damping")
 #ax.plot(t, y4[:,0], 'g', label="over damped")
 #ax.legend();
-
