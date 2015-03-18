248e772eba7625960d40ad7ea569102257f0464a
Richard Pang <rkp@d-128-95-10-223.dhcp4.washington.edu>
2015-03-18 09:39:53 -0700

    Change zeta to beta. Make magnitude of random force a settable parameter at start of code. Edit random driving force so that it can create a vector force.

--- oscillator.py
+++ oscillator.py
@@ -1,38 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Damped harmonic oscillator agent model
-
 Motion for the damped oscillator is described as:
-
 m*(d^2x/dt^2) + 2*zeta*w0*(dx/dt) + w0^2*x + [driving forces] = 0
-
 where x is the positon of the oscillator in one dimension, w0 i the frequency,
 zeta is the damping coefficient, and m is mass.
-
 w0 is the natural frequency of the harmonic oscillator, which is:
 w0 = sqrt(k/m)
-
 Driving forces are to be implemented (TODO). The first will be a uniform force
 in a random direction: F(t). The second will be bias, which will be a function
 of the temperature at the current position at the current time: b(T(x,t)).
-
 A second order ODE (odeint) is used to solve for the position of the mass,
 presently in 1D.
-
 End goal: run in 2d, add driving forces (random or otherwise) and add spatial
 and/or temperature-stimulus Bias.
-
 Created on Mon Mar 16 16:22:47 2015
 @authors: Richard Decal, decal@uw.edu
         Sharri Zamore
-
 https://staff.washington.edu/decal/
 https://github.com/isomerase/
-
 Code forked from Lecture-3-Scipy.ipynb from the SciPy lectures:
 https://github.com/jrjohansson/scientific-python-lectures
 authored by J.R. Johansson (robert@riken.jp) http://dml.riken.jp/~rob/
-
 Variable names have been changed to reflect terms in the original equations.
 """
 from scipy.integrate import odeint, ode # TODO: delete ode? unused... -rd
@@ -44,8 +33,8 @@ import numpy as np
 m = 1.0   # mass of agent
 k = 0.001   # spring constant
 w0 = np.sqrt(k/m)
-zeta = 0   # maybe rename? I don't like using the same name for the ode input and the function -sz
-
+beta = .1   # maybe rename? I don't like using the same name for the ode input and the function -sz
+force_mag = .000001
 
 # TODO: make Bias a class? Talk to Rich P about a smart way to make this object oriented. -sz
 
@@ -59,19 +48,24 @@ x0 = [1.0, 0.0]  # position_0, velocity_0
 
 # Time coodinates to solve the ODE for
 dt = 1  # timebin width
-runtime = 1000
+runtime = 500
 num_dt = runtime/dt  # number of timebins
 t = np.linspace(0, runtime, num_dt)
 
 
-def randomDrivingForce():
+def randomDrivingForce(dim=1):
     """Will eventually be a 2D unit-vector. For now, using just the x-component
     since we are only working in 1 dimension.
     """
-    rand_radians = np.random.uniform(0, 2*np.pi)  # high bound is not inclusive
-    x_component = 0.000001 * np.cos(rand_radians)
-    y_component = 0.000001 * np.sin(rand_radians)
-    return x_component
+    if dim == 1: # pick random direction in 1D
+        direction = np.random.choice([-1, 1])
+    elif dim == 2: # pick random direction in 2D
+        direction = np.random.uniform(0, 2*np.pi)  # high bound is not inclusive
+    elif dim == 3:
+        raise NotImplementedError('Three-dimensional model not implemented yet!')
+    force = force_mag * direction
+
+    return force
 
 
 def tempNow():
@@ -82,7 +76,6 @@ def tempNow():
 def biasedDrivingForce():
     """biased driving force, determined by temperature-stimulus at the current
     current position at the current time: b(T(x,t)).
-
     TODO: implement
     """
     return 0
@@ -91,20 +84,25 @@ def biasedDrivingForce():
 def MassAgent(init_state, t):
     """
     The right-hand side of the damped oscillator ODE
-    (d^2x/dt^2) = ( 2*zeta*w0*(dx/dt) + w0^2*x ) / m
-
+    (d^2x/dt^2) = - ( 2*beta*w0*(dx/dt) + w0^2*x ) / m
     TODO: why does this fxn need the time vector? it doesn't seem to use it.
         or is it because we will need it later for the driving forces..? -rd
+        - time is needed if you have any time-dependent driving force,
+        which we probably won't have for a while as our driving force will 
+        either be random or previously calculated from the temperature - rp
     """
     #This function gets entered into the ode, which is time-dependent.
-    x, dxdt = init_state[0], init_state[1]
-    #y, dydt = y0[0], y0[1] #starting to think about 2D..
+    r, drdt = init_state[0], init_state[1]
+    try:
+        dim = len(r)
+    except:
+        dim = 1
     
     #originally p = dx/dt, this was modified to include timesetp values
     #i feel like user-defined dt should be in the equations below...not sure -sz
-    dxddt = (-2 * zeta * w0 * dxdt - w0**2 * x + randomDrivingForce() + biasedDrivingForce()) / m 
+    drdt2 = (-2 * beta * w0 * drdt - w0**2 * r + randomDrivingForce(dim=dim) + biasedDrivingForce()) / m 
 
-    return [dxdt, dxddt]
+    return [drdt, drdt2]
 
 # TODO: change the ode such that if zeta is an n x 1 array, it produces n solution arrays
 
@@ -116,10 +114,12 @@ odeint basically works like this:
 4) iterate steps 2 and 3 to calc subsequent timesteps, using the x, xd, xdd
     from the previous dt to calculate the state values for the current dt
 ...
-
 then, it outputs the system states [x, xd](t)
 """
-states1 = odeint(MassAgent, x0, t)
+try:
+    states1 = odeint(MassAgent, x0, t)
+except:
+    import pdb; pdb.set_trace()
 
 fig, ax = plt.plot(t, states1)
 plt.xlabel('time')
@@ -130,7 +130,6 @@ plt.legend(('$x$', '$\dot{x}$'))
 
 def StateSpaceAnim():
     """Animation of changes in state-space over time.
-
     Credit to Paul Gribble (email: paul [at] gribblelab [dot] org), code based
     on a function in his "Computational Modelling in Neuroscience" course:
     http://www.gribblelab.org/compneuro/index.html
