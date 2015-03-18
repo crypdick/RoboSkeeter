0326a9dc0a50bee9caa404fb1b2ba20b10109aa7
rdecal <decal@uw.edu>
2015-03-17 22:25:03 -0700

    added random force

--- oscillator.py
+++ oscillator.py
@@ -4,13 +4,17 @@ Damped harmonic oscillator agent model
 
 Motion for the damped oscillator is described as:
 
-m*(d^2x/dt^2) + 2*zeta*w0*(dx/dt) + w0^2*x = 0
+m*(d^2x/dt^2) + 2*zeta*w0*(dx/dt) + w0^2*x + [driving forces] = 0
 
 where x is the positon of the oscillator in one dimension, w0 i the frequency,
 zeta is the damping coefficient, and m is mass.
 
 w0 is the natural frequency of the harmonic oscillator, which is:
 w0 = sqrt(k/m)
+
+Driving forces are to be implemented (TODO). The first will be a uniform force
+in a random direction: F(t). The second will be bias, which will be a function of
+the temperature at the current position at the current time: b(T(x,t)).
 
 A second order ODE (odeint) is used to solve for the position of the mass,
 presently in 1D.
@@ -31,18 +35,19 @@ authored by J.R. Johansson (robert@riken.jp) http://dml.riken.jp/~rob/
 
 Variable names have been changed to reflect terms in the original equations.
 """
-from scipy.integrate import odeint, ode
+from scipy.integrate import odeint, ode # TODO: delete ode? unused... -rd
 from matplotlib import pyplot as plt
 import numpy as np
 
-# CONSTANTS. TODO: make user-input. TODO: all caps for constant naming conventions
+# CONSTANTS. TODO: make user-input.-sz
+# TODO: all caps for constant naming conventions -rd
 m = 1.0   # mass of agent
 k = 0.001   # spring constant
 w0 = np.sqrt(k/m)
-zeta = 0   # maybe rename? I don't like using the same name for the ode input and the function
+zeta = 0   # maybe rename? I don't like using the same name for the ode input and the function -sz
 
 
-# TODO: make Bias a class? Talk to Rich P about a smart way to make this object oriented.
+# TODO: make Bias a class? Talk to Rich P about a smart way to make this object oriented. -sz
 
 # Initial state of the spring.
 x0 = [1.0, 0.0]  # position_0, velocity_0
@@ -54,6 +59,14 @@ dt = 1  # timebin width
 runtime = 1000
 num_dt = runtime/dt  # number of timebins
 t = np.linspace(0, runtime, num_dt)
+
+def randomForce():
+    """Will eventually be a 2D unit-vector. For now, using just the x-component
+    since we are only working in 1 dimension.    
+    """
+    rand_radians = np.random.uniform(0, 2*np.pi) # high bound is not inclusive
+    rand_x = 0.000001 * np.cos(rand_radians)
+    return rand_x
 
 
 def MassAgent(init_state, t):
@@ -70,7 +83,7 @@ def MassAgent(init_state, t):
     
     #originally p = dx/dt, this was modified to include timesetp values
     #i feel like user-defined dt should be in the equations below...not sure -sz
-    dxddt = (-2 * zeta * w0 * dxdt - w0**2 * x) / m 
+    dxddt = (-2 * zeta * w0 * dxdt - w0**2 * x + randomForce()) / m 
 
     return [dxdt, dxddt]
 
