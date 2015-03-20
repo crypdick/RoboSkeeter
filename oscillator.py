af13074939b5dae9be86b7eec02d88e1a00db9f3
rdecal <decal@uw.edu>
2015-03-20 14:13:39 -0700

    outsourced plotting fxns to its own script

--- oscillator.py
+++ oscillator.py
@@ -41,9 +41,9 @@ https://github.com/jrjohansson/scientific-python-lectures
 authored by J.R. Johansson (robert@riken.jp) http://dml.riken.jp/~rob/
 """
 from scipy.integrate import odeint, ode  # TODO: delete ode? unused... -rd
-from matplotlib import pyplot as plt
 import numpy as np
 from pylab import gca
+import agent_plotting_funcs
 
 # CONSTANTS. TODO: make user-input.-sz
 # TODO: all caps for constant naming conventions -rd
@@ -184,72 +184,14 @@ def run_ODE(t):
         return states1
     except:
         import pdb; pdb.set_trace()  # TODO: wtf? -rd
-    
-    
-def StatesOverTimeGraph(t, states):
-    plt.figure(1) #linewidth=2
-    plt.plot(t, states[:, 0], color = 'b', linewidth=2)
-    plt.plot(t, states[:, 1], color = 'g', linewidth=1, linestyle=':')
-    plt.plot(t, states[:, 2], color = 'r', linewidth=2)
-    plt.plot(t, states[:, 3], color = 'cyan', linewidth=1, linestyle=':')
-    plt.xlabel('time')
-    plt.ylabel('states')
-    plt.title('mass-agent oscillating system')
-    if dim == 1:
-        plt.legend(('x', 'vx'))
-    elif dim == 2:
-        plt.legend(('x', 'vx', 'y', 'vy'))
-
-
-def StateSpaceDraw(states, dim=1, animate=False, box=False):
-    """Plot state-space over course of experiment.
-    
-    Credit to Paul Gribble (email: paul [at] gribblelab [dot] org), code based
-    on a function in his "Computational Modelling in Neuroscience" course:
-    http://www.gribblelab.org/compneuro/index.html
-    """
-    plt.figure(2)
-    if dim == 1:
-        pb, = plt.plot(states[:, 0], states[:, 1], 'b-')
-        plt.xlabel('x')
-        plt.ylabel('vx')
-#        p, = plt.plot(states1[0:10, 0], states1[0:10, 1], 'b-')
-#        pp, = plt.plot(states1[10, 0], states1[10, 1], 'b.', markersize=10)
-        tt = plt.title("State-space graph for 1 dimensions")
-    elif dim == 2:
-        
-        pb, = plt.plot(states[:, 0], states[:, 2], 'black', linewidth=2)  # select cols 0,2
-                                                            # for x,y pos
-        plt.xlabel('x position')
-        plt.ylabel('y position')
-#        p, = plt.plot(states1[0:10, 0], states1[0:10, 2], 'b-')
-#        pp, = plt.plot(states1[10, 0], states1[10, 2], 'b.', markersize=10)
-        tt = plt.title("State-space graph for 2 dimensions")
-        if box is True:
-#                plt.ylim(-0,5, 0.5)
-#                plt.xlim(-0,5, 0.5)
-                plt.grid()
-#                gca().add_patch(pylab.Rectangle((-0.5, -0.5), 1, 1, linewidth=3, fill=False))
-
-    # animate
-    if animate is True:
-        step = 2
-        for i in xrange(1, np.shape(states1)[0]-10, step):
-            p.set_xdata(states1[10+i:20+i, 0])
-            p.set_ydata(states1[10+i:20+i, 1])
-            pp.set_xdata(states1[10+i, 0])
-            pp.set_ydata(states1[10+i, 1])
-            tt.set_text("State-space trajectory animation - step # %d" % (i))
-            plt.draw()
 
      
 def main(runtime = 2e3, dt=1, plotting=True):
-    plt.close('all')
     dt, runtime, t = def_time_coords(runtime, dt)
     states = run_ODE(t)
     if plotting is True:
-        StatesOverTimeGraph(t, states)
-        StateSpaceDraw(states, dim=dim, animate=False, box=True)
+        agent_plotting_funcs.StatesOverTimeGraph(t, states)
+        agent_plotting_funcs.StateSpaceDraw(states, dim=dim, animate=False, box=True)
     return states
 
 if __name__ == '__main__':
