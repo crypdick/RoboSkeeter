1794ce5641337b81b7065f1e1d19fdb54f6ffb45
Ricardo Decal <richard.decal@ncf.edu>
2015-03-25 00:22:44 -0700

    +plot of all trajectories

--- trajgen_flightstats.py
+++ trajgen_flightstats.py
@@ -11,6 +11,7 @@ https://github.com/isomerase/
 """
 import traj_gen
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
@@ -22,6 +23,7 @@ def trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
     accels = []
     source_finds = []
     t_finds = []
+    agent_paths_fig = plt.figure(0)
 
     for i in range(total_trajectories):
         t, r, v, a, source_found, tfound = traj_gen.traj_gen(r0=r0, v0=v0, k=k, beta=beta, f0=f0, wf0=wf0, rs=rs, Tmax=Tmax, dt=dt)
@@ -30,6 +32,10 @@ def trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
         accels += [a]
         source_finds += [source_found]
         t_finds += [tfound]
+        plt.plot(r[:, 0], r[:, 1], lw=2, alpha=0.5)
+        plt.scatter(rs[0], rs[1], s=25, c='r', lw=0)
+    plt.title("agent trajectories")  # TODO: list params
+    plt.show()
 
     return pos, velos, accels, source_finds, np.array(t_finds)
 
@@ -38,14 +44,14 @@ def stateHistograms(pos, velos, accels):
     pos_all = np.concatenate(pos, axis=0)
     posHistBinWidth = 0.1
     position_lim = 2.0
-    positional_bins = np.arange(-position_lim-posHistBinWidth, position_lim + posHistBinWidth, posHistBinWidth) 
+    positional_bins = np.arange(-position_lim-posHistBinWidth, position_lim + posHistBinWidth, posHistBinWidth)
     pos_dist_fig = plt.figure(1)
     plt.hist(pos_all)
 #    plt.hist(pos_all[:,0], bins=positional_bins, alpha=0.5, label='x', normed=True)
 #    plt.hist(pos_all[:,1], bins=positional_bins, alpha=0.5, label='y', normed=True)
     plt.title("x,y position distributions")
     plt.legend()
-    
+
     velo_all = np.concatenate(velos, axis=0)
     veloHistBinWidth = 0.1
     velo_lim = 0.5
@@ -54,9 +60,9 @@ def stateHistograms(pos, velos, accels):
     plt.hist(velo_all)
 #    plt.hist(velo_all[:,0], bins=velo_bins, alpha=0.5, label='vx', normed=True)
 #    plt.hist(velo_all[:,1], bins=velo_bins, alpha=0.5, label='vy', normed=True)
-    plt.title("x,y velocity distributions")    
+    plt.title("x,y velocity distributions")
     plt.legend()
-    
+
     accel_all = np.concatenate(accels, axis=0)
     accelHistBinWidth = 0.1
     accel_lim = 0.5
@@ -67,30 +73,29 @@ def stateHistograms(pos, velos, accels):
 #    plt.hist(accel_all[:,1], bins=accel_bins, alpha=0.5, label='ay', normed=True)
     plt.title("x,y acceleration distributions")
     plt.legend()
-    
+
     plt.show()
-    
+
 
 def probFindGrid(source_finds):
     pass
 
 
-def T_find_average(t_finds):
+def T_find_average(t_finds, total_trajectories):
     t_finds_NoNaNs = t_finds[~np.isnan(t_finds)]  # remove NaNs
     Tfind_avg = sum(t_finds_NoNaNs)/len(t_finds_NoNaNs)
-    print("<Time_find> = ", Tfind_avg)
-    return Tfind_avg
+    print "<Time_find> = ", Tfind_avg, "sec. ", len(t_finds_NoNaNs), "finds out of ", total_trajectories, "trajectories"
+    return Tfind_avg  # TODO: append to trajectory title
 
-def main(r0, v0, k, beta, f0, rs, Tmax, dt, total_trajectories):
-    pos, velos, accels, source_finds, t_finds = trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories)
-    Tfind_avg = T_find_average(t_finds)
+
+def main(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
+    pos, velos, accels, source_finds, t_finds = trajGenIter(r0=r0, v0=v0, k=k, beta=beta, f0=f0, wf0=wf0, rs=rs, Tmax=Tmax, dt=dt, total_trajectories=total_trajectories)
+    Tfind_avg = T_find_average(t_finds, total_trajectories)
 
     return pos, velos, accels, source_finds, Tfind_avg
 
+
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    
-    pos, velos, accels, source_finds, Tfind_avg = main(r0=[1., 0], v0=[0, 0.4], k=1e-6, beta=2e-7, f0=3e-6, rs = [0.13, 0.01], Tmax=30.0, dt=0.01, total_trajectories=15)
+    pos, velos, accels, source_finds, Tfind_avg = main(r0=[1., 0], v0=[0, 0.4], k=1e-6, beta=2e-7, f0=3e-6, wf0=5e-7, rs = [0.13, 0.01], Tmax=30.0, dt=0.01, total_trajectories=15)
     stateHistograms(pos, velos, accels)
     probFindGrid(source_finds)
-
