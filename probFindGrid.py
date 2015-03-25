d73a91f946cbb9c02dcf2b36d4d7aba0de01935d
Ricardo Decal <richard.decal@ncf.edu>
2015-03-25 10:01:14 -0700

    initial commit

---
+++ probFindGrid.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Creates a grid and places the stimulus in each grid cell. Then, runs many flight
+trajectories for each stimulus location and displays the probabiltiy P_find 
+at each cell as a heatmap.
+
+Created on Wed Mar 25 09:59:21 2015
+
+@author: richard
+"""
+
+def probFindGrid(source_finds):
+    pass
