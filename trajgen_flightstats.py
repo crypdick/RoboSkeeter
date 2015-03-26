3a3704a85730ff82a0f4539e5cb077d78066cb33
Ricardo Decal <richard.decal@ncf.edu>
2015-03-25 21:31:33 -0700

    added absolute velocity and acceleration plots

--- trajgen_flightstats.py
+++ trajgen_flightstats.py
@@ -2,9 +2,6 @@
 """
 Fork of flight_stats code to use the traj_gen script instead
 
-TODO: make r0, v0 vary
-TODO: make sure globals inside traj_gen get overriden (e.g. Tmax)
-
 @author: Richard Decal, decal@uw.edu
 https://staff.washington.edu/decal/
 https://github.com/isomerase/
@@ -26,7 +23,7 @@ def trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
     agent_paths_fig = plt.figure(1)
 
     for i in range(total_trajectories):
-        v0 = np.random.normal(0, 0.3, 2)
+        v0 = np.random.normal(0, 0.2, 2)
         t, r, v, a, source_found, tfound = traj_gen.traj_gen(r0=r0, v0=v0, k=k, beta=beta, f0=f0, wf0=wf0, rs=rs, Tmax=Tmax, dt=dt)
         pos += [r]
         velos += [v]
@@ -35,7 +32,9 @@ def trajGenIter(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
         t_finds += [tfound]
         plt.plot(r[:, 0], r[:, 1], lw=2, alpha=0.5)
         plt.scatter(rs[0], rs[1], s=150, c='r', marker="*")
-    plt.title("agent trajectories")  # TODO: list params
+    plt.title("Agent trajectories")  # TODO: list params
+    plt.xlabel("x position")
+    plt.ylabel("y position")
     plt.savefig("agent trajectories.png")
     plt.show()    
 
@@ -66,18 +65,37 @@ def stateHistograms(pos, velos, accels):
     plt.title("x,y velocity distributions")
     plt.legend()
     plt.savefig("velocity distributions histo.png")
+    # absolute velo
+    abs_velo_dist_fig = plt.figure(4)
+    velo_all_magn = []
+    for v in velo_all:
+        velo_all_magn.append(np.linalg.norm(v))
+    plt.hist(velo_all_magn, label='v_total', bins=20, normed=True)
+    plt.title("absolute velocity distributions")
+    plt.legend()
+    plt.savefig("absolute velocity distributions histo.png")
+    
 
     accel_all = np.concatenate(accels, axis=0)
     accelHistBinWidth = 0.6
     accel_lim = 9.
     accel_bins = np.arange((-accel_lim - accelHistBinWidth), (accel_lim + accelHistBinWidth), accelHistBinWidth)
-    accel_dist_fig = plt.figure(4)
+    accel_dist_fig = plt.figure(5)
 #    plt.hist(accel_all)
     plt.hist(accel_all[:,0], bins=accel_bins, alpha=0.5, label='ax', normed=True)
     plt.hist(accel_all[:,1], bins=accel_bins, alpha=0.5, label='ay', normed=True)
     plt.title("x,y acceleration distributions")
     plt.legend()
     plt.savefig("acceleration distributions histo.png")
+    # absolute accel
+    abs_accel_dist_fig = plt.figure(6)
+    accel_all_magn = []
+    for a in accel_all:
+        accel_all_magn.append(np.linalg.norm(a))
+    plt.hist(accel_all_magn, label='a_total', bins=20, normed=True)
+    plt.title("absolute acceleration distributions")
+    plt.legend()
+    plt.savefig("absolute acceleration distributions histo.png")
 
     plt.show()
 
@@ -85,7 +103,7 @@ def stateHistograms(pos, velos, accels):
 def T_find_average(t_finds, total_trajectories):
     t_finds_NoNaNs = t_finds[~np.isnan(t_finds)]  # remove NaNs
     if len(t_finds_NoNaNs) == 0:
-        print "No successful source finds!"
+        print "No successful source finds out of %s trajectories!" % total_trajectories
     else:
         Tfind_avg = sum(t_finds_NoNaNs)/len(t_finds_NoNaNs)
         print "<Time_find> = ", Tfind_avg, "sec. ", len(t_finds_NoNaNs), "finds out of ", total_trajectories, "trajectories"
@@ -102,14 +120,14 @@ def main(r0, v0, k, beta, f0, wf0, rs, Tmax, dt, total_trajectories):
 if __name__ == '__main__':
     # set default params to send to main
     r0 = [1., 0]
-    v0 = [0, 0.2]  # made this draw from a 2D gaussian above
+    v0 = [0, 0.2]  # ignore, these will get drawn from a 2D gaussian above
     k = 1e-5
     beta = 2e-5
-    f0 = 0. #5e-6
-    wf0 = 0. #1e-6
+    f0 = 5e-6
+    wf0 = 1e-6
     rs = [0.2, 0.05]
     Tmax = 5.0
     dt = 0.01
-    total_trajectories = 50
+    total_trajectories = 2
     pos, velos, accels, source_finds, Tfind_avg = main(r0=r0, v0=v0, k=k, beta=beta, f0=f0, wf0=wf0, rs=rs, Tmax=Tmax, dt=dt, total_trajectories=total_trajectories)
     stateHistograms(pos, velos, accels)
