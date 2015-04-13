e8282d850c2720e7c059ba3bdb7cfcd14f0a847e
Richard Decal <decal@uw.edu>
2015-04-13 15:07:25 -0700

    added wall force field + outsourced plotting funcs

--- Agent Model/generate_trajectory.py
+++ Agent Model/generate_trajectory.py
@@ -3,13 +3,12 @@ Created on Mon Mar 23 15:07:40 2015
 
 @author: rkp, rbd
 
-Generate "mosquito" trajectories (class object) using harmonic oscillator 
-equations.
+Generate "mosquito" trajectories (class object) using harmonic oscillator equations.
 """
 
 import numpy as np
 from numpy.linalg import norm
-import matplotlib.pyplot as plt
+import plotting_funcs
 
 ## define params
 # population weight data: 2.88 +- 0.35mg
@@ -26,6 +25,8 @@ def random_force(rf, dim=2):
     Returns:
         random force x and y components (array)
     """
+    if rf == 0.:
+        return np.array([0., 0.])
     if dim == 2:
         mag = np.random.exponential(rf)
         theta = np.random.uniform(high=2*np.pi)
@@ -56,22 +57,40 @@ def upwindBiasForce(wtf, upwind_direction=0, dim=2):
         raise NotImplementedError('wind bias only works in 2D right now!')
 
 
-def wall_force_field(current_pos, wallF, wallF_exp, wallX_pos=[0., 1.], wallY_pos=[-0.15, 0.15]):
-    """If agent gets too close to wall, inflict it with repulsive forces as a
-    function of how close it is to the wall. NOTE: right now, just using simply
-    forbidden zones.
+def wall_force_field(current_pos, current_velo, wallF, wallX_pos=[0., 1.], wallY_pos=[-0.15, 0.15]):
+    """If agent gets too close to wall, inflict it with repulsive forces as a function of how close it is to the wall and it's speed towards the wall.
     
     Args:
         current_pos: (x,y) coords of agent right now (array)
-        wallF: how scary the wall is (float)
-        wallF_exp: the exponential term in the wall force field (float)
+        wallF: (None | tuple)
+            None- disabled
+            tuple- (lambda (float), y0 (float). Lambda is the decay constant in the exponentially decaying wall repulsive force, y0 is where the decaying starts.
         wallX_pos: Wall X coords (array)
         wallY_pos: Wall Y coords (array)
     
     Returns:
         wall_force: (array)
     """
-    pass
+    if wallF is None:
+        return [0., 0.]
+#    elif type(wallF) == "tuple":
+    else:
+        wallF_lambda, wallF_y0 = wallF
+        wallF_x = 0.
+        wallF_y = 0.
+        posx, posy = current_pos
+        velox, veloy = current_velo
+        y_dist = 0.15 - abs(posy) # distance of agent from wall
+        force_mag_y = wallF_magnitude(y_dist, wallF_lambda, wallF_y0)
+        if (posy < 0 and veloy < 0) or (posy > 0 and veloy > 0): # heading towards top OR bottom
+            wallF_y = -1. * veloy * force_mag_y  # equal an opposite force, scaled by force mag
+        return np.array([wallF_x, wallF_y])
+
+def wallF_magnitude(distance, wallF, y0 = 1.):
+    """magnitude of wall repulsion, a function of position
+    
+    """
+    return y0 * np.exp(-1 * wallF * distance)
 
 
 def stimulusDrivingForce():
@@ -149,7 +168,7 @@ class Trajectory:
         trajectory object
     """
     boundary = [0.0, 1.0, 0.15, -0.15]  # these are real dims of our wind tunnel
-    def __init__(self, agent_pos, v0_stdev, Tmax, dt, target_pos, beta, rf, wtf, detect_thresh, bounded, bounce, plotting=False, k=0.):
+    def __init__(self, agent_pos, v0_stdev, Tmax, dt, target_pos, beta, rf, wtf, detect_thresh, bounded, bounce, wallF, plotting=False, titleappend = '', k=0.):
         """ Initialize object with instant variables, and trigger other funcs. 
         """
         self.Tmax = Tmax
@@ -161,6 +180,7 @@ class Trajectory:
         self.wtf = wtf
         self.detect_thresh = detect_thresh     
         self.bounce = bounce
+        self.wallF = wallF
         
         # place heater
         self.target_pos = place_heater(target_pos)
@@ -190,14 +210,15 @@ class Trajectory:
         self.fly(ts_max, detect_thresh, self.boundary, bounded)
         
         if plotting is True:
-            self.plot(self.boundary)
+            self.titleappend = titleappend
+            plotting_funcs.plot_single_trajectory(self.positionList, self.target_pos, self.detect_thresh, self.boundary, self.titleappend)
         
     def fly(self, ts_max, detect_thresh, boundary, bounded):
         """Run the simulation using Euler's method"""
         ## loop through timesteps
         for ts in range(ts_max-1):
             # calculate current force
-            force = -self.k*self.positionList[ts] - self.beta*self.veloList[ts] + random_force(self.rf) + upwindBiasForce(self.wtf) #+ wall_force_field(self.positionList[ts])
+            force = -self.k*self.positionList[ts] - self.beta*self.veloList[ts] + random_force(self.rf) + upwindBiasForce(self.wtf) + wall_force_field(self.positionList[ts], self.veloList[ts], self.wallF)
             # calculate current acceleration
             self.accelList[ts] = force/m
     
@@ -221,10 +242,12 @@ class Trajectory:
                 if candidate_pos[1] > boundary[2]:  # too far up
                     candidate_pos[1] = boundary[2] + 1e-4
                     if self.bounce is "crash":
+#                        print "teleport!"
                         self.veloList[ts+1][1] = 0.
                 elif candidate_pos[1] < boundary[3]:  # too far down
                     candidate_pos[1] = boundary[3] - 1e-4
                     if self.bounce is "crash":
+#                        print "teleport!"
                         self.veloList[ts+1][1] = 0.
                 
             self.positionList[ts+1] = candidate_pos
@@ -246,24 +269,9 @@ class Trajectory:
         self.positionList = self.positionList[:ts+1]
         self.veloList = self.veloList[:ts+1]
         self.accelList = self.accelList[:ts+1]
-         
-    def plot(self, boundary):
-        from matplotlib.patches import Rectangle
-        plt.plot(self.positionList[:, 0], self.positionList[:, 1], lw=2, alpha=0.5)
-        heaterCircle = plt.Circle((self.target_pos[0], self.target_pos[1],), 0.003175, color='r')  # 0.003175 is diam of our heater
-        detectCircle = plt.Circle((self.target_pos[0], self.target_pos[1],), self.detect_thresh, color='gray', fill=False, linestyle='dashed')
-        plt.axis(boundary)
-        # draw cage
-        cage_midX, cage_midY = 0.1524, 0.
-        currentAxis = plt.gca()
-        currentAxis.add_patch(Rectangle((cage_midX - 0.0381, cage_midY - 0.0381), 0.0762, 0.0762, facecolor='none'))
-        currentAxis.add_artist(heaterCircle)
-        currentAxis.add_artist(detectCircle)
-        plt.title("Individual trajectory", fontsize=20)
-        plt.xlabel("$x$", fontsize=14)
-        plt.ylabel("$y$", fontsize=14)
+
 
 if __name__ == '__main__':
     target_pos = "left"
     
-    mytraj = Trajectory(agent_pos="cage", target_pos="left", plotting = True,   v0_stdev=0.01, wtf=7e-07, rf=4e-06, beta=1e-5, Tmax=15, dt=0.01, detect_thresh=0.023175, bounded=True, bounce="crash")
+    mytraj = Trajectory(agent_pos="cage", target_pos="left", plotting = True, v0_stdev=0.01, wtf=7e-07, rf=4e-06, beta=1e-5, Tmax=15, dt=0.01, detect_thresh=0.023175, bounded=True, bounce="crash", wallF=(80, 1e-4))
