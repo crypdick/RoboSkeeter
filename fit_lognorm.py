aa1b0424024d44d3a02e8ee99a4edb4783220aab
Richard Decal <decal@uw.edu>
2015-04-15 22:22:35 -0700

    fit Dickinson data to lognormal

--- fit_lognorm.py
+++ fit_lognorm.py
@@ -8,14 +8,18 @@ Created on Tue Apr  7 23:07:31 2015
 from scipy import stats
 import numpy as np
 from matplotlib import pyplot as plt
+import dick_pickle
+
+# grab odor off info
+odor_off = dick_pickle.main(plotting=False)
 
 binspots = odor_off["acceleration"]['abs']['bin_centers']
 normed_counts = odor_off["acceleration"]['abs']['normed_cts']
 
 # plot Dickinson acceleration magnitudes
-plt.plot(binspots, normed_counts, lw=2, label='Dickinson data')
-plt.legend()
-plt.show()
+#plt.plot(binspots, normed_counts, lw=2, label='Dickinson data')
+#plt.legend()
+#plt.show()
 
 # normed_counts is set of probabilities, each one corresponding to a certain acceleration bin
 # so, we must draw a bunch of random samples from the probability distribution
@@ -29,15 +33,20 @@ geom_mean = np.exp(mu) # Geometric mean == median
 geom_stdev = np.exp(sigma) # Geometric standard deviation
 
 # Plot fit
+#plt.plot(binspots, stats.lognorm.pdf(binspots, shape, loc=0, scale=scale), 'r', linewidth=3, label='fit')
+#plt.legend()
+#plt.show()
+
+# plot together
+#plt.plot(binspots, normed_counts, lw=2, label='Dickinson data')
 plt.plot(binspots, stats.lognorm.pdf(binspots, shape, loc=0, scale=scale), 'r', linewidth=3, label='fit')
+#plt.plot(binspots, stats.lognorm.pdf(binspots, shape, loc=0, scale=.1), 'g', linewidth=3, label='test')
 plt.legend()
 plt.show()
 
-# plot together
-plt.plot(binspots, normed_counts, lw=2, label='Dickinson data')
-plt.plot(binspots, stats.lognorm.pdf(binspots, shape, loc=0, scale=scale), 'r', linewidth=3, label='fit')
-plt.legend()
-plt.show()
+#plt.plot(binspots, stats.lognorm.pdf(binspots, shape, loc=0, scale=.1), 'g', linewidth=3, label='test')
+#plt.xlim([0,1])
+#plt.show()
 
 # print results
 print "shape/sigma, standard dev of log(X) = ", sigma
@@ -62,5 +71,8 @@ print "Geometric mean / median = ", geom_mean
 x = np.linspace(0,10,200)
 pdf = (np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2))
         / (x * sigma * np.sqrt(2 * np.pi)))
-#y = np.random.lognormal(mean=mu, sigma=sigma, size=scale)
+y = np.random.lognormal(mean=mu, sigma=sigma, size=scale)
 plt.plot(x, pdf)
+plt.plot(x, y)
+
+y = np.random.lognormal(mean=mu, sigma=sigma, size=scale)
