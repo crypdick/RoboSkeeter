1812f405989bb6e1019fdc012435339268dc9b8b
Richard Decal <decal@uw.edu>
2015-05-07 11:57:53 -0700

    fixed some plume bugs

--- agent_model/plume.py
+++ agent_model/plume.py
@@ -9,29 +9,39 @@ Created on Thu Apr 23 10:53:11 2015
 
 from scipy.spatial import cKDTree
 import pandas as pd
+import numpy as np
 
 
 #boundary = [2.0, 2.3, 0.15, -0.15, 1]  # these are real dims of our wind tunnel
 
 
 def mkplume():
-    plume = pd.read_csv('temperature_data/COMSOL2D_temp.csv')
+    ''' Genearate a plume based on a saved file.
+    
+    Input:
+    none
+    
+    Output:
+    Plume: (pandas dataframe, cols (x,y,temp))
+    '''
+    plume = pd.read_csv('temperature_data/LH_50_tempfield-10s.csv')
     # grab just last timestep of the plume
-    plume = plume.loc[:,['x', 'y', 't348']]
-    plume.rename(columns={'t348': 'temp'}, inplace=True)
+    plume = plume.loc[:,['x', 'y', 'T2 (K) @ t=30']]
+    plume.rename(columns={'T2 (K) @ t=30': 'temp'}, inplace=True)
     # correct for model convention
     plume['y'] = plume.y.values - 0.127
     return plume
 
 
 def mk_tree(plume):
+    ''' Creates a k-d tree map of the pandas Dataframe to look up temperatures
+    '''
     return cKDTree(plume[['x', 'y']])
-#    return 0
 
-#def templookup(coords):
-#    return 0
 
 class Plume():
+    ''' Instantiates a plume object.
+    '''
     def __init__(self):
         self.plume = mkplume()
         self.plume_kdTree = mk_tree(self.plume)
@@ -52,8 +62,15 @@ class Plume():
         return self.plume.loc[index, 'temp']
         
     def show(self):
-        pass
-        #TODO: plot plume
+        ''' TODO plot the plume
+        '''
+        from matplotlib import pyplot as plt
+        
+        ax = self.plume.plot(kind='hexbin', x='x', y='y', C='temp', reduce_C_function=np.max,
+                        gridsize=(20,60), vmax=297, title ="Temperature inside wind tunnel (K)", ylim=[-0.127, 0.127])
+        ax.set_aspect('equal')
+        ax.invert_yaxis()
+
         
     
 
