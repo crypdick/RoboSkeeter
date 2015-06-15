610926f445d12969e578323997263dd3623ca0b6
Richard Decal <decal@uw.edu>
2015-06-15 12:36:17 -0700

    cleaned + commented. need to check that Gingl_plane is properly implemented

--- agent_model/Ginglsensor.py
+++ agent_model/Ginglsensor.py
@@ -8,18 +8,22 @@ sensor neuron that models sensor neurons from
 Gingl et al 2004 doi:10.1152/jn.01152/jn.01164.2004
 
 input:
-sensory value/time, e.g.  CO2 intensity d/dt
+sensory value/time, e.g.  temp intensity d/dt
 
 output:
 spike rate
+
+
+TODO: need to verify that this is all reasonable
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from random import random, seed
 from matplotlib import cm
 
-def Ginglplane():
+
+def show_Gingl_plane():
     """F = -63.2415 + 3.7504 T + 326.8702 dT/dt 
     3.7504 T + 326.8702 dT/dt - F - 63.2415 = 0
     
@@ -27,18 +31,20 @@ def Ginglplane():
     T between 30-24
     dT/dt between +- 0.03
     F between 25- 55
+    
+    TODO: make sure that the surface code is properly implemented
     """
-    point1  = np.array([29,0,50])
-    normal1 = np.array([3.7504,326.8702, -1])
+    point1  = np.array([29, 0, 50])
+    normal1 = np.array([3.7504, 326.8702, -1])
     
     # a plane is a*x+b*y+c*z+d=0
     # [a,b,c] is the normal. Thus, we have to calculate
     # d and we're set
 #    d1 = -np.sum(point1*normal1)# dot product 
-    d1 = -63.2415
+    d1 = -63.2415 # TODO: wtf?
     
     # create x,y
-    xx, yy = np.meshgrid(np.linspace(31,24.5,100), np.linspace(-0.02,0.02, 100)) #ys should be from -.03 to .03
+    xx, yy = np.meshgrid(np.linspace(31, 24.5,100), np.linspace(-0.02,0.02, 100)) #ys should be from -.03 to .03
     
     # calculate corresponding z
     z1 = (-normal1[0]*xx - normal1[1]*yy - d1)*1./normal1[2]   
@@ -54,19 +60,9 @@ def Ginglplane():
     
     # plot the surface
     plt3d = plt.figure().gca(projection='3d')
-    plt3d.plot_surface(xx,yy,z1, cmap=theCM)
+    plt3d.plot_surface(xx, yy, z1, cmap=theCM)
     
-#    #make a shdow of the plane on the wall
-#    cset = plt3d.contour(xx,yy,z1, zdir='z',  cmap=theCM)
-#    cset = plt3d.contour(xx,yy,z1, zdir='x',  cmap=theCM)
-#    cset = plt3d.contour(xx,yy,z1, zdir='y',  cmap=theCM)
     
-#    plt3d.set_xlabel('X')
-#    plt3d.set_xlim(-24, 29)
-#    plt3d.set_ylabel('Y')
-#    plt3d.set_ylim(-0.3, 0.3)
-#    plt3d.set_zlabel('Z')
-#    plt3d.set_zlim(25, 50)
     plt.show() 
 
 
@@ -77,10 +73,11 @@ def gingl_neuron(intensity_ddt,temp):
     F = -63.2415 + 3.7504 * temp + 326.8702 * intensity_ddt
     print "fire rate at temp = " + str(temp) + " and d temp/dt = " + str(intensity_ddt) + " ==> " + str(F)
 
+
 def main():  
     intensity_ddt = 0.0
     temp = 29.0
-    Ginglplane()
+    show_Gingl_plane()
     gingl_neuron(intensity_ddt, temp)
 
 if __name__ == "__main__":
