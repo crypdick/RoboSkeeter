89df169035d7f6574a3cbaec456464a506c070d2
Richard Decal <decal@uw.edu>
2015-07-28 17:59:26 -0700

    new velocity csvs, trying basin hopping since fmin keeps converging to BS

--- agent_model/param_optimize.py
+++ agent_model/param_optimize.py
@@ -62,7 +62,7 @@ def wrapper(GUESS):
         detect_thresh=0.023175,
         bounded=True,
         wallF_params=wallF_params)
-    myagent.fly(total_trajectories=20, verbose=False)
+    myagent.fly(total_trajectories=1, verbose=False)
 
     ensemble = trajectories.ensemble
     trimmed_ensemble = ensemble.loc[
@@ -81,32 +81,37 @@ def error_fxn(ensemble):
     # compare ensemble to experiments, return score to wrapper
 
     # get histogram vals for ensemble
-    dist = 0.1
+    adist = 0.1
     # |a|
-    amin, amax = -4., 6.+dist  # arange drops last number, so pad range by dist
+    amin, amax = -4., 6.+adist  # arange drops last number, so pad range by dist
     # pdb.set
     accel_all_magn = ensemble['acceleration_3Dmagn'].values
-    aabs_counts, aabs_bins = np.histogram(accel_all_magn, bins=np.arange(amin, amax, dist))
+    aabs_counts, aabs_bins = np.histogram(accel_all_magn, bins=np.arange(amin, amax, adist))
     if np.isnan(np.sum(aabs_counts)) is True:
         print "NANANAN"
     aabs_counts = aabs_counts.astype(float)
     aabs_counts_n = aabs_counts / aabs_counts.sum()
 
-    vmin, vmax = -1., 1.+dist
+    vdist =  0.015
+    vmin, vmax = -0.7, 0.7
     velo_all_magn = ensemble['velocity_3Dmagn'].values
-    vabs_counts, vabs_bins = np.histogram(velo_all_magn, bins=np.arange(vmin, vmax, dist))
+    vabs_counts, vabs_bins = np.histogram(velo_all_magn, bins=np.arange(vmin, vmax, vdist))
     vabs_counts = vabs_counts.astype(float)
     vabs_counts_n = vabs_counts / vabs_counts.sum()
 
     # print csv
     # print aabs_counts_n, 'counts',
     accel_error = np.sum(abs(aabs_counts_n - a_observed)) * 100000  # multiply score to get better granularity
-    velocity_error = np.sum(abs(vabs_counts_n - v_observed)) * 400000 # there are fewer residuals for velo, so we multiply it more
+    velocity_error = np.sum(abs(vabs_counts_n - v_observed)) * 100000
+    if np.isnan(accel_error) or np.isnan(velocity_error):
+        print "a counts", aabs_counts, "\n \n"
+        print "a n", aabs_counts_n
 
     final_score = accel_error + velocity_error
     global HIGH_SCORE
     if final_score < HIGH_SCORE:
         HIGH_SCORE = final_score
+        print "Bingo! New high score: {}".format(HIGH_SCORE)
 
         plt.ioff()
         plt.figure()
@@ -128,10 +133,10 @@ def error_fxn(ensemble):
 
 # # for nelder mead
 # Nfeval = 1
-# def callbackF(Xi):
-#     global Nfeval
-#     print '{0:4d}   {1: 3.25f}   {2: 3.25f}   {3: 3.8f}'.format(Nfeval, Xi[0], Xi[1], wrapper(Xi))
-#     Nfeval += 1
+def callbackF(Xi):
+     global Nfeval
+     print '{0:4d}   {1: 3.25f}   {2: 3.25f}   {3: 3.8f}'.format(Nfeval, Xi[0], Xi[1], wrapper(Xi))
+     Nfeval += 1
 
 # # for basin hopping
 # Nfeval = 1
@@ -161,24 +166,25 @@ def main():
     #     disp=3)
 
     print "Starting optimizer."
-    result = fmin(
-        wrapper,
-        [412, 288, 500], # [1.0239e-5, 2.88e-6,  5e-6]
-        xtol=20,
-        full_output=1,
-        disp=1,
-        retall=1
-        # ftol=0.0001
-    )
+#    result = fmin(
+#        wrapper, # (bias, mass, damping)
+#        [412, 288, 500], # [1.0239e-5, 2.88e-6,  5e-6]
+#        xtol=20,
+#        full_output=1,
+#        disp=1,
+#        retall=1
+#        # ftol=0.0001
+#    )
 
-    # result = basinhopping(
-    #     wrapper,
-    #     [640],
-    #     stepsize=1000,
-    #     T=4000,
-    #     minimizer_kwargs={"bounds": ((1, 10000),)},
-    #     callback=callbackF,
-    #     disp=True)
+    result = basinhopping(
+         wrapper,
+         [412, 288, 500],
+         stepsize=500,
+         T=4000,
+         minimizer_kwargs={"bounds": ((200, 1000),(255,1000),(200,1000))},
+         callback=callbackF,
+         disp=True
+         )
 
     return result
     # fminbound(wrapper, )
