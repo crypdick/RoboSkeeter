19b63ce5ac48966c440e465d5f2c5da73098ee10
Richard Decal <decal@uw.edu>
2015-09-09 10:48:13 -0700

    refactoring; better documentation

--- agent_model/agent3D.py
+++ agent_model/agent3D.py
@@ -120,8 +120,7 @@ class Agent():
         Agent object
 
     """
-    def __init__(
-        self,
+    def __init__(self,
         Trajectory_object,
         Plume_object,
         Windtunnel_object,
@@ -137,7 +136,8 @@ class Agent():
         bounded=True,
         mass = 3e-6, #3.0e-6 # 2.88e-6  # mass (kg) =2.88 mg,
         k=0.):
-        
+        """generate the agent"""
+
         self.metadata = {}
 
         # population weight data: 2.88 +- 0.35mg
@@ -176,19 +176,20 @@ class Agent():
         # self._repulsion_funcs = repulsion_landscape3D.landscape(boundary=self.metadata['boundary'])
 
 
+
     def fly(self, total_trajectories=1, verbose=True):
         ''' iterates self._fly_single() total_trajectories times
         '''
-        traj_count = 0
         args = (self.metadata['time bindwidth'], self.metadata['mass'], self.metadata['boundary'])
         df_list = []
+
+        traj_count = 0
         while traj_count < total_trajectories:
             if verbose is True:
                 sys.stdout.write("\rTrajectory {}/{}".format(traj_count+1, total_trajectories))
-                sys.stdout.flush()
+                # sys.stdout.flush()
 
-
-            kinematics_dict = self._fly_single(*args)
+            kinematics_dict = self._generate_flight(*args)
             kinematics_dict['trajectory_num'] = traj_count  # enumerate the trajectories
             df = pd.DataFrame(kinematics_dict)
 
@@ -209,53 +210,46 @@ class Agent():
         # concluding stats
 #        self.trajectory_obj.add_agent_info({'time_target_find_avg': trajectory3D.T_find_stats(self.trajectory_obj.agent_info['time_to_target_find'])})
     
-    def _fly_single(self, dt, m, boundary, bounded=True):
+    def _generate_flight(self, dt, m, boundary, collision = "elastic", bounded=True):
         """Generate a single trajectory using our model.
     
         First put everything into np arrays stored inside of a dictionary
         """
-        BOUNCE = "elastic"
         PLUME_TRIGGER_TIME = int(1.0/dt)
-        tsi_max = int(np.ceil(self.metadata['time_max'] / dt))  # N bins/maximum time step
-        
-        # V for vector
-        # retrieve dict w/ vars(V)
-        # V = Object()
+        max_bins = int(np.ceil(self.metadata['time_max'] / dt))  # N bins
+
+        ################################################
+        # initialize np arrays, store in dictionary
+        ################################################
         V = {}
         
         for name in self.metadata['kinematic_vals']+self.metadata['forces']:
-            # adds attribute to V
             for ext in ['_x', '_y', '_z', '_xy_theta', '_xy_mag']:
-                # setattr(V, name+ext, np.full(tsi_max, np.nan))
-                V[name+ext] = np.full(tsi_max, np.nan)
-        
-        
-        
-        # initialize np arrays
-        
-        V['times'] = np.linspace(0, self.metadata['time_max'], tsi_max)
-        V['position_x'] = np.full(tsi_max, np.nan)
-        V['position_y'] = np.full(tsi_max, np.nan)
-        V['position_z'] = np.full(tsi_max, np.nan)
-        V['inPlume'] = np.full(tsi_max, -1, dtype=np.uint8)
-        V['plume_experience'] = [None]*tsi_max
-        V['turning'] = [None]*tsi_max
-        V['heading_angle'] = np.full(tsi_max, np.nan)
-        V['velocity_angular'] = np.full(tsi_max, np.nan)
+                V[name+ext] = np.full(max_bins, np.nan)
 
-        ######## INITIAL CONDITIONS
+        V['times'] = np.linspace(0, self.metadata['time_max'], max_bins)
+        V['position_x'] = np.full(max_bins, np.nan)
+        V['position_y'] = np.full(max_bins, np.nan)
+        V['position_z'] = np.full(max_bins, np.nan)
+        V['inPlume'] = np.full(max_bins, -1, dtype=np.uint8)
+        V['behavior_state'] = [None]*max_bins
+        V['turning'] = [None]*max_bins
+        V['heading_angle'] = np.full(max_bins, np.nan)
+        V['velocity_angular'] = np.full(max_bins, np.nan)
+
+        ################################################
+        # Set INITIAL CONDITIONS
+        ################################################
         # place agent
         V['position_x'][0], V['position_y'][0], V['position_z'][0] = place_agent(self.metadata['initial_position'])
 
         # generate random intial velocity condition using normal distribution fitted to experimental data
         V['velocity_x'][0], V['velocity_y'][0], V['velocity_z'][0] = np.random.normal(0, self.metadata['initial_velo_stdev'], 3)
 
-        # generate a flight!
-        for tsi in xrange(tsi_max):
-            # import pdb; pdb.set_trace()
-            # sense the temperature
-#            _temperature[tsi] = self.plume_obj.temp_lookup([_position_x[tsi], _position_y[tsi]]) # depreciated
-
+        ################################################
+        # Generate flight
+        ################################################
+        for tsi in xrange(max_bins):
             # are we in the plume?
             if self.plume_obj.condition is None:  # skip if no plume
                 V['inPlume'][tsi] = 0
@@ -263,37 +257,41 @@ class Agent():
                 V['inPlume'][tsi] = self.plume_obj.check_for_plume(
                     [V['position_x'][tsi], V['position_y'][tsi], V['position_z'][tsi]])
 
-            ############ what is our behavior given our plume interactions thus far?
+            ################################################
+            # Figure out behavioral state
+            ################################################
             if tsi == 0:
-                V['plume_experience'][tsi] = 'searching'
+                V['behavior_state'][tsi] = 'searching'
             else:
                 if self.plume_obj.condition is None:  # hack for no plume condition
-                    V['plume_experience'][tsi] = 'searching'
+                    V['behavior_state'][tsi] = 'searching'
                 else:
-                    if V['plume_experience'][tsi] is None: # need to find state
-                        V['plume_experience'][tsi] = self._check_crossing_state(tsi, V['inPlume'], V['velocity_y'][tsi-1])
-                        # import pdb; pdb.set_trace()
-                        if V['plume_experience'][tsi] in (
+                    if V['behavior_state'][tsi] is None: # need to find state
+                        V['behavior_state'][tsi] = self._check_crossing_state(tsi, V['inPlume'], V['velocity_y'][tsi-1])
+                        if V['behavior_state'][tsi] in (
                                 'Left_plume Exit leftLeft_plume Exit rightRight_plume Exit leftRight_plume Exit right'):
                             try:
                                 for i in range(PLUME_TRIGGER_TIME): # store experience for the following timeperiod
-                                    V['plume_experience'][tsi+i] = str(V['plume_experience'][tsi])
+                                    V['behavior_state'][tsi+i] = str(V['behavior_state'][tsi])
                             except IndexError: # can't store whole snapshot, so save 'truncated' label instead
                                # print "plume trigger turn lasted less than threshold of {} timesteps "\
-                               # "before trajectory ended, so adding _truncated suffix".format(tsi_max)
-                               for i in range(tsi_max - tsi):
-                                    V['plume_experience'][tsi+i] = (str(V['plume_experience'][tsi])+'_truncated')
+                               # "before trajectory ended, so adding _truncated suffix".format(max_bins)  # TODO: DOCUMENT
+                               for i in range(max_bins - tsi):
+                                    V['behavior_state'][tsi+i] = (str(V['behavior_state'][tsi])+'_truncated')
 
-                    else: # if we're already orienting from past memory
+                    else: # our state is already set from past experience
                         if V['inPlume'] is False:  # we haven't re-entered the plume
                             pass
                         else:  # found the plume again!
-                            V['plume_experience'][tsi:]
+                            V['behavior_state'][tsi:] = None  # reset memory
+                            V['behavior_state'][tsi] = 'entering'
             ######################################################################
 
 
-            # calculate driving forces' direction
-            F_stim = stim_biasF3D.stimF(V['plume_experience'][tsi], self.metadata['stimF_strength'])
+            ################################################
+            # Calculate driving forces at this timestep
+            ################################################
+            F_stim = stim_biasF3D.stimF(V['behavior_state'][tsi], self.metadata['stimF_strength'])
             V['stimF_x'][tsi], V['stimF_y'][tsi], V['stimF_z'][tsi] = F_stim
 
             F_bias = baseline_driving_forces3D.random_force(self.metadata['randomF_strength'])
@@ -306,7 +304,9 @@ class Agent():
             V['wallRepulsiveF_x'][tsi], V['wallRepulsiveF_y'][tsi], V['wallRepulsiveF_z'][tsi] = F_wall_repulsion
 
 
-            ########################### calculate current force
+            ################################################
+            # calculate total force
+            ################################################
             F_driving =  F_bias + F_upwind + F_wall_repulsion + F_stim
             F_damping = -self.metadata['beta']*np.array([V['velocity_x'][tsi], V['velocity_y'][tsi], V['velocity_z'][tsi]])
             F_total = F_damping + F_driving
@@ -317,23 +317,25 @@ class Agent():
             V['acceleration_x'][tsi], V['acceleration_y'][tsi], V['acceleration_z'][tsi] = F_total / m
 
             # if time is out, end loop before we solve for future velo, position
-            if tsi == tsi_max-1: # -1 because of how range() works
+            if tsi == max_bins-1: # -1 because of how range() works
 #                self.metadata['target_found'][0]  = False
 #                self.metadata['time_to_target_find'][0] = np.nan
                 V = self.land(tsi, V)
                 break
 
-            # calculate future velocity given the forces we just got
+            ################################################
+            # Calculate candidate velocity and positions
+            ################################################
             V['velocity_x'][tsi+1], V['velocity_y'][tsi+1], V['velocity_z'][tsi+1] = \
                 np.array([V['velocity_x'][tsi], V['velocity_y'][tsi], V['velocity_z'][tsi]]) \
                 + np.array([V['acceleration_x'][tsi], V['acceleration_y'][tsi], V['acceleration_z'][tsi]]) * dt
 
-
-            # solve candidate position for next timestep
             candidate_pos = np.array([V['position_x'][tsi], V['position_y'][tsi], V['position_z'][tsi]]) \
                 + np.array([V['velocity_x'][tsi], V['velocity_y'][tsi], V['velocity_z'][tsi]])*dt # shouldnt position in future be affected by the forces in this timestep? aka use velo[i+1]
-#
-            # if walls are enabled, forbid mosquito from going out of bounds
+
+            ################################################
+            # if walls are enabled, check if candidate velocity and position is illegal
+            ################################################
             if bounded is True:
                 # x dim
                 if candidate_pos[0] > boundary[1]:  # reached far (upwind) wall (end)
@@ -344,52 +346,55 @@ class Agent():
                 if candidate_pos[0] < boundary[0]:  # too far left
 #                    print "too far left"
                     candidate_pos[0] = boundary[0] + 1e-4 # teleport back inside
-                    if BOUNCE == 'elastic':
+                    if collision == 'elastic':
                         V['velocity_x'][tsi+1] *= -1
 #                        print "boom! left"
-                    elif BOUNCE == 'crash':
+                    elif collision == 'crash':
                         V['velocity_x'][tsi+1] = 0.
 
                 #y dim
                 if candidate_pos[1] > boundary[2]:  # too left
 #                    print "too left"
                     candidate_pos[1] = boundary[2] + 1e-4 # note, left is going more negative in our convention
-                    if BOUNCE == 'elastic':
+                    if collision == 'elastic':
                         V['velocity_y'][tsi+1] *= -1
-                    elif BOUNCE == "crash":
+                    elif collision == "crash":
 #                        print "teleport!"
                         V['velocity_y'][tsi+1] = 0.
 #                        print "crash! top wall"
                 if candidate_pos[1] < boundary[3]:  # too far right
 #                    print "too far right"
                     candidate_pos[1] = boundary[3] - 1e-4
-                    if BOUNCE == 'elastic':
+                    if collision == 'elastic':
                         V['velocity_y'][tsi+1] *= -1
-                    elif BOUNCE == 'crash':
+                    elif collision == 'crash':
                         V['velocity_y'][tsi+1] = 0.
 
                 # z dim
                 if candidate_pos[2] > boundary[5]:  # too far above
 #                    print "too far above"
                     candidate_pos[2] = boundary[5] - 1e-4
-                    if BOUNCE == 'elastic':
+                    if collision == 'elastic':
                         V['velocity_z'][tsi+1] *= -1
 #                        print "boom! top"
-                    elif BOUNCE == "crash":
+                    elif collision == "crash":
 #                        print "teleport!"
                         V['velocity_z'][tsi+1] = 0.
 #                        print "crash! top wall"
                 if candidate_pos[2] < boundary[4]:  # too far below
 #                    print "too far below"
                     candidate_pos[2] = boundary[4] + 1e-4
-                    if BOUNCE == 'elastic':
+                    if collision == 'elastic':
                         V['velocity_z'][tsi+1] *= -1
-                    elif BOUNCE == 'crash':
+                    elif collision == 'crash':
                         V['velocity_z'][tsi+1] = 0.
 
-                # save screened candidate_pos to future position
             V['position_x'][tsi+1], V['position_y'][tsi+1], V['position_z'][tsi+1] = candidate_pos
+            ###########################################################################################################
 
+            ################################################
+            # solve for heading angle, turning state, etc. TODO: export to trajectories class
+            ################################################
             # solve final heading #TODO: also solve zy?
             V['heading_angle'][tsi] = solve_heading(V['velocity_y'][tsi], V['velocity_x'][tsi])
             # ... and angular velo
@@ -415,7 +420,7 @@ class Agent():
         ''' trim excess timebins in arrays
         '''
         for array in array_dict.itervalues():
-            array = array[:tsi+1]
+            array = array[:tsi]
 
         array_dict = self._calc_polar_kinematics(array_dict)
 
@@ -437,7 +442,8 @@ class Agent():
         in2in - staying
         in2out - exiting
             {Left_plume Exit left, Left_plume Exit right
-#           Right_plume Exit left, Right_plume Exit right}
+            Right_plume Exit left, Right_plume Exit right}
+        TODO: export to trajectory class
         """
         current_state, past_state = inPlume[tsi], inPlume[tsi-1]
         if current_state == 0 and past_state == 0:
@@ -462,9 +468,9 @@ class Agent():
                 else:
                     return "Right_plume Exit right"
             
-            
+
     def _calc_polar_kinematics(self, array_dict):
-        """append polar kinematics to vectors dictionary"""
+        """append polar kinematics to vectors dictionary TODO: export to trajectory class"""
         for name in self.metadata['kinematic_vals']+self.metadata['forces']:  # ['velocity', 'acceleration', 'biasF', 'wallRepulsiveF', 'upwindF', 'stimF']
             x_component, y_component = array_dict[name+'_x'], array_dict[name+'_y']
             angle = np.arctan2(y_component, x_component)
@@ -476,20 +482,11 @@ class Agent():
 
 
 
-def main():
+def gen_objects_and_fly(N_TRAJECTORIES, TEST_CONDITION, BETA, FORCES_AMPLITUDE, F_WIND_SCALE, K):
     """
     Params fitted using scipy.optimize
 
     """
-
-    N_TRAJECTORIES = 1
-    TEST_CONDITION = None  # {'Left', 'Right', None}
-    # old beta- 5e-5, forces 4.12405e-6, fwind = 5e-7
-    BETA, FORCES_AMPLITUDE, F_WIND_SCALE =  [  1.37213380e-06  , 1.39026239e-06 ,  2.06854777e-08]
-    MASS = 2.88e-6
-    F_STIM_SCALE = 0.  #7e-7,   # set to zero to disable tracking hot air
-    K = 0.  #1e-7               # set to zero to disable wall attraction
-
     # generate environment
     windtunnel_object = windtunnel.Windtunnel(TEST_CONDITION)
     # generate temperature plume
@@ -508,7 +505,7 @@ def main():
         stimF_str=F_STIM_SCALE,
         k=K,
         beta=BETA,
-        Tmax=15.,
+        Tmax=4.,
         dt=0.01,
         bounded=True)
     sys.stdout.write("\rAgent born")
@@ -525,12 +522,20 @@ def main():
     
 
 if __name__ == '__main__':
-    myplume, trajectories, skeeter = main()
+    N_TRAJECTORIES = 3
+    TEST_CONDITION = None  # {'Left', 'Right', None}
+    # old beta- 5e-5, forces 4.12405e-6, fwind = 5e-7
+    BETA, FORCES_AMPLITUDE, F_WIND_SCALE =  [  1.37213380e-06  , 1.39026239e-06 ,  7.06854777e-07]
+    MASS = 2.88e-6
+    F_STIM_SCALE = 0.  #7e-7,   # set to zero to disable tracking hot air
+    K = 0.  #1e-7               # set to zero to disable wall attraction
+
+    myplume, trajectories, skeeter = gen_objects_and_fly(N_TRAJECTORIES, TEST_CONDITION, BETA, FORCES_AMPLITUDE, F_WIND_SCALE, K)
 
     print "\nDone."
 
     ######################### plotting methods
-    # trajectories.plot_single_3Dtrajectory(0)  # plot ith trajectory in the ensemble of trajectories
+    trajectories.plot_single_3Dtrajectory(0)  # plot ith trajectory in the ensemble of trajectories
 
     # trajectories.plot_force_violin()
     # trajectories.plot_kinematic_hists()
@@ -552,4 +557,4 @@ if __name__ == '__main__':
     # # trajectories.plot_force_violin()
     #
     # # # for plume stats
-    # # g = e.loc[e['plume_experience'].isin(['Left_plume Exit left', 'Left_plume Exit right', 'Right_plume Exit left', 'Right_plume Exit right'])]
+    # # g = e.loc[e['behavior_state'].isin(['Left_plume Exit left', 'Left_plume Exit right', 'Right_plume Exit left', 'Right_plume Exit right'])]
