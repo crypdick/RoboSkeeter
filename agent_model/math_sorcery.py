e0a5b34c77086f9ae55158d498dcc84ec8142c28
Richard Decal <decal@uw.edu>
2015-09-10 21:13:29 -0700

    refactoring (consolidating scripts)

--- agent_model/math_sorcery.py
+++ agent_model/math_sorcery.py
@@ -14,4 +14,19 @@ def calculate_curvature(self): #TODO check if np arrays
     for i in range(len(v_x)):
         k_i = np.abs( np.cross(velo_vector, accel_vector) ) /                           \
             (np.linalg.norm([a_x[i], a_y[i], a_z[i]]) ** 3)
-    $ TODO: output?
+    # TODO: output?
+
+def gen_symm_vecs(dims, N):
+    """generate radially-symmetric vectors sampled from the unit circle.  These
+    can then be scaled by a force to make radially symmetric distributions.
+
+    making a scatter plot of many draws from this function makes a unit circle.
+
+    credit: http://codereview.stackexchange.com/a/77945/76407
+    """
+    vecs = np.random.normal(size=dims)
+    mags = linalg.norm(vecs, axis=-1)
+
+    ends = vecs / mags[..., newaxis]  # divide by length to get unit vector
+
+    return ends
