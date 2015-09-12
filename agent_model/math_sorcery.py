ecebafd407dfe14fe493c5b8044f5fb82a630778
Richard Decal <decal@uw.edu>
2015-09-11 17:49:43 -0700

    more documentation

--- agent_model/math_sorcery.py
+++ agent_model/math_sorcery.py
@@ -9,15 +9,18 @@ def calculate_heading(velo_x_component, velo_y_component):
 
 
 def calculate_curvature(ensemble):
+    """using formula from https://en.wikipedia.org/wiki/Curvature#Local_expressions_2"""
     velo_vector = np.vstack((ensemble.velocity_x, ensemble.velocity_y, ensemble.velocity_z)  # shape is (3, R)
     accel_vector = np.vstack((ensemble.acceleration_x, ensemble.acceleration_y, ensemble.acceleration_z))
-    # using formula from https://en.wikipedia.org/wiki/Curvature#Local_expressions_2
      return np.abs( np.cross(velo_vector, accel_vector) ) /                           \
             np.linalg.norm(accel_vector, axis=0)** 3
 
 
-def gen_symm_vecs(dims):
-    """sample random (radially-symmetric) 3D vectors from the unit sphere.
+def gen_symm_vecs(dims=3):
+    """generate randomly pointed (radially-symmetric) 3D unit vectors/ direction vectors
+
+    first we draw from a 3D gaussian, which is a symmetric distribution no matter how you slice it. then, we map
+    those draws onto the unit sphere.
 
     credit: http://codereview.stackexchange.com/a/77945/76407
     """
