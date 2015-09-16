2271baaed9cecedb2bc1ae75560935b848bc0796
Richard Decal <decal@uw.edu>
2015-09-16 16:48:30 -0700

    added heater classes, fixed plotting funcs to work with them

--- agent_model/windtunnel.py
+++ agent_model/windtunnel.py
@@ -1,7 +1,7 @@
 __author__ = 'richard'
 
 class Windtunnel():
-    def __init__(self, test_condition, collision='elastic'):
+    def __init__(self, test_condition, collision_type='elastic'):
         """
         boundary: (array)
             specify where walls are  (minx, maxx, miny, maxy)
@@ -9,11 +9,21 @@ class Windtunnel():
         """
         self.boundary = [0.0, 1.0, 0.127, -0.127, 0., 0.254]  # these are real dims of our wind tunnel
         self.test_condition = test_condition
-        # place heater
-        self.place_heater(test_condition)
-        self.collision = collision
+        self.collision_type = collision_type
 
-    def place_heater(self, test_condition):
+        self.left_heater = Heater('left')
+        self.left_heater_loc = self.left_heater.xy_coords
+
+        self.right_heater = Heater('right')
+        self.right_heater_loc = self.right_heater.xy_coords
+
+        self.on_heater = Heater(test_condition)
+        if self.on_heater is not None:
+            self.on_heater_loc = self.on_heater.xy_coords
+
+
+class Heater():
+    def __init__(self, side):
         ''' given {left, right, none, custom coords} place heater in the windtunnel
 
         Args:
@@ -21,17 +31,17 @@ class Windtunnel():
 
         returns [x,y, zmin, zmax, diam]
         '''
-        zmin = 0.03800
-        zmax = 0.11340
-        diam = 0.01905
-        if test_condition is None:
-            self.heater_location = None
-        elif test_condition in "leftLeftLEFT":
-            self.heater_location = [0.8651, -0.0507, zmin, zmax, diam, 'left']
-        elif test_condition in "rightRightRIGHT":
-            self.heater_location =  [0.8651, 0.0507, zmin, zmax, diam, 'right']
-        elif type(test_condition) is list:
-            self.heater_location = test_condition
+        self.zmin = 0.03800
+        self.zmax = 0.11340
+        self.diam = 0.01905
+        self.side = side
+
+        if side is None:
+            self.xy_coords =  None
+        elif side in "leftLeftLEFT":
+            self.xy_coords =  [0.8651, -0.0507, self.zmin, self.zmax, self.diam]
+        elif side in "rightRightRIGHT":
+            self.xy_coords =   [0.8651, 0.0507, self.zmin, self.zmax, self.diam]
         else:
             raise Exception('invalid location type specified')
 
