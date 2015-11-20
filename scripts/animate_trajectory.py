b53c7b9e8f383ee3c0fae1d4c17543a18cf84657
Richard Decal <decal@uw.edu>
2015-11-19 18:11:43 -0800

    generalized script; fixed saving

--- scripts/animate_trajectory.py
+++ scripts/animate_trajectory.py
@@ -3,23 +3,26 @@ from matplotlib import pyplot as plt
 
 import scripts.plot_windtunnel as pwt
 
-sim_or_exp = 'simulation'
-trajectory_i = None
+sim_or_exp = 'experiment'  # 'experiment', 'simulation'
+trajectory_i = 8  # None
 highlight_inside_plume = False
-show_plume = True
+show_plume = False
 
 experiment = eval(sim_or_exp)
 
 if trajectory_i is None:
-    trajectory_i = trajectory_s.get_trajectory_numbers().min()
+    trajectory_i = experiment.trajectories.get_trajectory_numbers().min()
+
+df = experiment.trajectories.get_trajectory_i_df(trajectory_i)
+
+p = df[['position_x', 'position_y', 'position_z']].values
+x_t = p.reshape((1, len(p), 3))
+
 
 fig, ax = pwt.plot_windtunnel(experiment.windtunnel)
 ax.axis('off')
 if show_plume:
     pwt.draw_plume(experiment, ax=ax)
-
-p = trajectory_s.data[['position_x', 'position_y', 'position_z']].values
-x_t = p.reshape((trajectory_s.data.trajectory_num.unique().__len__(), len(p), 3))
 
 # set up lines and points
 lines = sum([ax.plot([], [], [], '-', c='gray')
@@ -34,7 +37,7 @@ pts = sum([ax.plot([], [], [], '*', c='black')
 # ax.set_zlim((0, .254))
 
 # set point-of-view: specified by (altitude degrees, azimuth degrees)
-# ax.view_init(30, 0)
+# ax.view_init(90, 0)
 
 # initialization function: plot the background of each frame
 def init():
@@ -67,9 +70,11 @@ def animate(i):
 
 # instantiate the animator.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               interval=1, blit=True, repeat_delay=8000)
+                               interval=1, blit=False, repeat_delay=8000, frames=len(p))
 
 # Save as mp4. This requires mplayer or ffmpeg to be installed
-# anim.save('lorentz_attractor.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=100, metadata=dict(artist='Me'), bitrate=1000)
+anim.save('{}-{}.mp4'.format(sim_or_exp, trajectory_i), writer=writer)
 
 plt.show()
