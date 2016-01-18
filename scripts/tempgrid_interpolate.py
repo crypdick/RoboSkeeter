5b4aabffb9485f87940a4217d4dde3a9a7eebb26
Richard Decal <decal@uw.edu>
2016-01-17 17:23:47 -0800

    taking out hardcoded dirs

--- scripts/tempgrid_interpolate.py
+++ scripts/tempgrid_interpolate.py
@@ -10,11 +10,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator, griddata
 import pandas as pd
+from scripts.i_o import get_directory
 
 
-tempdata_fname = '/home/richard/src/RoboSkeeter/data/experiments/plume_data/thermocouple/timeavg_right/timeavg_right.csv'
+temp_data_fname = get_directory('THERMOCOUPLE_TIMEAVG_RIGHT_CSV')
 # time averaged temperature data (m x 4 matrix: xpos, ypos, zpos, temp)
-tempdata = np.genfromtxt(tempdata_fname, delimiter=',')
+temp_data = np.genfromtxt(temp_data_fname, delimiter=',')
 
 # windtunnel dimensions
 #      self.left = -0.127
@@ -23,9 +24,10 @@ tempdata = np.genfromtxt(tempdata_fname, delimiter=',')
 #      self.downwind = 0.0
 #      self.ceiling = 0.25
 #      self.floor = 0.
-grid_x, grid_y, grid_z = np.mgrid[0.:1.:100j, -0.127:0.127:25j, 0:0.254:25j]
-points = tempdata[:,:3]  # (1382, 3)
-temps = tempdata[:, 3]  # len 1382
+grid_x, grid_y, grid_z = np.mgrid[0.:1.:10j, -0.127:0.127:6j, 0:0.254:3j]
+# grid_x, grid_y, grid_z = np.mgrid[0.:1.:100j, -0.127:0.127:25j, 0:0.254:25j]
+points = temp_data[:, :3]  # (1382, 3)
+temps = temp_data[:, 3]  # len 1382
 
 interpolated_temps = griddata(points,
                               temps,
@@ -38,12 +40,12 @@ spatial_grad_x, spatial_grad_y, spatial_grad_z = np.gradient(interpolated_temps,
                                                              grid_y,
                                                              grid_z)
 
-# norm = spatial_grad_x **2 + spatial_grad_y**2 + spatial_grad_z **2
+norm = spatial_grad_x **2 + spatial_grad_y**2 + spatial_grad_z **2
 
-# Visualize spatial gradient
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-ax.quiver(grid_x, grid_y, grid_z, spatial_grad_x, spatial_grad_y, spatial_grad_z, length=0.01)
-
-plt.show()
+# # Visualize spatial gradient
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+#
+# ax.quiver(grid_x, grid_y, grid_z, spatial_grad_x, spatial_grad_y, spatial_grad_z, length=0.01)
+#
+# plt.show()
