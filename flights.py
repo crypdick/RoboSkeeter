1b6be95c5ba5b8725648bca0ca6f34be8acc3662
Richard Decal <decal@uw.edu>
2016-04-02 22:54:48 -0600

    refactoring, other improvements

--- flights.py
+++ flights.py
@@ -17,21 +17,18 @@ trajectory.describe()
 trajectory.save
 """
 import os
-import string
-from analysis.kinematic_math import DoMath
+
 import numpy as np
 import pandas as pd
 
-
-from scripts import i_o, animate_trajectory_callable
+from scripts import i_o
 
 
 class Flights(object):
     def __init__(self):
         self.kinematics = pd.DataFrame()
-        self.percent_time_in_plume = None
 
-    def load_observations_and_analyze(self, dataframe_list):
+    def concat_df_list(self, dataframe_list):
         """
         Takes list of pandas dataframes, concatinates them, and runs analysis functions.
         Parameters
@@ -45,41 +42,27 @@ class Flights(object):
         """
         self.kinematics = pd.concat(dataframe_list)
 
-
     def dump2csvs(self):
         """we don't use self.flights.to_csv(name) because Sharri likes having separate csvs for each trajectory
+
+        Output
+        ------
+        Numbered csvs
         """
         for trajectory_i in self.kinematics.trajectory_num.unique():
-            temp_traj = self.get_trajectory_i_df(trajectory_i)
-            temp_array = temp_traj[['position_x', 'position_y']].values
+            temp_traj = self.get_trajectory_slice(trajectory_i)
+            temp_array = temp_traj[['position_x', 'position_y', 'position_z', 'in_plume']].values
             np.savetxt(str(trajectory_i) + ".csv", temp_array, delimiter=",")
 
-    def calc_side_ratio_score(self):
-        """upwind left vs right ratio"""  # TODO replace with KF score
-        upwind_half = self.kinematics.loc[self.kinematics.position_x > 0.5]
-        total_pts = len(upwind_half)
 
-        left_upwind_pts = len(self.kinematics.loc[(self.kinematics.position_x > 0.5) & (self.kinematics.position_y < 0)])
-        right_upwind_pts = total_pts - left_upwind_pts
-        print "seconds extra on left side: ", (left_upwind_pts - right_upwind_pts) / 100.
-        ratio = float(left_upwind_pts) / right_upwind_pts
-        print ratio
 
-    def _extract_number_from_fname(self, token):
-        extract_digits = lambda stng: "".join(char for char in stng if char in string.digits + ".")
-        to_float = lambda x: float(x) if x.count(".") <= 1 else None
-
-        try:
-            return to_float(extract_digits(token))
-        except ValueError:
-            print token, extract_digits(token)
-
-    def get_trajectory_i_df(self, index):
+    def get_trajectory_slice(self, index=None):
         """
         # TODO: select list of ints
         Parameters
         ----------
-        index (int or None)
+        index
+            (int or None)
             trajectory index you want to select
 
         Returns
@@ -95,62 +78,12 @@ class Flights(object):
 
         return df
 
-
     def get_trajectory_numbers(self):
         return np.sort(self.kinematics.trajectory_num.unique())
 
-
-    def load_experiments(self, experimental_condition):
-        dir_labels = {
-            'Control': 'EXP_TRAJECTORIES_CONTROL',
-            'Left': 'EXP_TRAJECTORIES_LEFT',
-            'Right': 'EXP_TRAJECTORIES_RIGHT'}
-        directory = i_o.get_directory(dir_labels[experimental_condition])
-
-        self.agent_obj = None
-
-        df_list = []
-
-        col_labels = [
-            'position_x',
-            'position_y',
-            'position_z',
-            'velocity_x',
-            'velocity_y',
-            'velocity_z',
-            'acceleration_x',
-            'acceleration_y',
-            'acceleration_z',
-            'heading_angleS',
-            'angular_velo_xyS',
-            'angular_velo_yzS',
-            'curvatureS'
-        ]
-
-        for fname in [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]:  # list files
-            print "Loading {} from {}".format(fname, directory)
-            file_path = os.path.join(directory, fname)
-            # base_name = os.path.splitext(file_path)[0]
-
-            dataframe = pd.read_csv(file_path, na_values="NaN", names=col_labels)  # recognize str(NaN) as NaN
-            dataframe.fillna(value=0, inplace=True)
-
-            df_len = len(dataframe.position_x)
-
-            # take fname number (slice out "control_" and ".csv")
-            fname_num = int(fname[:-4])
-
-            # ensure that csv fname is just an int
-            if type(fname_num) is not int:
-                raise ValueError('''we are expecting csv files to be integers.
-                    instead, we found type {}. culprit: {}'''.format(type(fname_num), fname))
-
-            dataframe['trajectory_num'] = [fname_num] * df_len
-            dataframe['tsi'] = np.arange(df_len)
-
-            df_list.append(dataframe)
-
-        self.load_observations_and_analyze(dataframe_list=df_list)
+    def experiment_data_to_DF(self, experimental_condition):
+        df = i_o.load_csv_to_df(experimental_condition)
+        self.kinematics = df
 
     def _trim_df_endzones(self):
         return self.kinematics.loc[(self.kinematics['position_x'] > 0.05) & (self.kinematics['position_x'] < 0.95)]
