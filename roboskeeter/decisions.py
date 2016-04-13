126a2dcf0324ca2a81cc8abb72b40ab50c46e776
Richard Decal <decal@uw.edu>
2016-04-13 16:27:26 -0500

    fleshed out decision policies

--- roboskeeter/decisions.py
+++ roboskeeter/decisions.py
@@ -1,6 +1,7 @@
 
 class Decisions:
     def __init__(self, plume, decision_policy, stimulus_memory_n_timesteps):
+        # we take plume as input for the gradient ascent. however, this is inelegant. FIXME
         self.plume = plume
         self.decision_policy = decision_policy
 
@@ -11,54 +12,65 @@ class Decisions:
         if 'cast' in decision_policy or 'surge' in decision_policy:
             # sanity check
             if plume.plume_model != 'Boolean':
-                raise TypeError('expecting a boolean plume')
+                raise TypeError('expecting a boolean plume model')
             self.make_decision = self._boolean_decisions
         elif 'gradient' in decision_policy:
             # sanity check
             if plume.plume_model != 'TimeaAvg':  # TODO add other types
-
+                raise TypeError("expecting gradient plume model")
             self.make_decision = self._gradient_decisions
 
 
     def _boolean_decisions(self, in_plume, crosswind_velocity):
-        current_decision = ''  # reset
-
         if in_plume is True:
             self.plume_sighted_ago = 0
-            plume_interaction = 'in'
+            plume_signal = 'in'
             if 'surge' in self.decision_policy:
                 current_decision = 'surge'
+            else:
+                current_decision = 'search'
         elif in_plume is False:
             self.plume_sighted_ago += 1
             if self.plume_sighted_ago == 1:  # we just exited the plume
                 # if our y velocity is negative, we just exited to the left. otherwise, to the right.
                 if crosswind_velocity < 0:
-                    plume_interaction = 'exit left'
+                    plume_signal = 'exit_l'
                     self.last_plume_side_exited = 'l'
+                    current_decision = 'cast_r'
                 else:
-                    plume_interaction = 'exit right'
+                    plume_signal = 'exit_r'
                     self.last_plume_side_exited = 'r'
-            else:
-                plume_interaction = 'out'
+                    current_decision = 'cast_l'
+            else:  # been outside plume at least a couple timesteps
+                plume_signal = 'out'
                 if 'cast' in self.decision_policy:
                     if self.plume_sighted_ago <= self.stimulus_memory_n_timesteps:  # we were in the plume recently
                         if self.last_plume_side_exited == 'l':
                             current_decision = 'cast_r'
                         else:  # 'r'
                             current_decision = 'cast_l'
-                else:
+                else:  # haven't seen the plume in a while
                     current_decision = 'search'
 
-        return current_decision, plume_interaction
+        return current_decision, plume_signal
 
 
     def _gradient_decisions(self, position):
-            plume_interaction = self.plume.get_nearest_gradient(position)
-            # TODO
-            # if gradient is above thresh, 'ascend'
-            #else: search
-            # surge up gradient
-        else:
-            raise TypeError
+        """
 
-        return current_decision, plume_interaction
+        Parameters
+        ----------
+        position
+            current position. used to lookup gradient
+
+        Returns
+        -------
+        current_decision
+            we are always following the gradient in this model
+        plume_signal
+            the gradient
+        """
+        plume_signal = self.plume.get_nearest_gradient(position)
+        current_decision = 'ga'
+
+        return current_decision, plume_signal
