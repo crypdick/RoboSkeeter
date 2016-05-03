95489e4720922cb2036162d9d4e1576d19cbfa44
Richard Decal <decal@uw.edu>
2016-05-03 16:30:51 -0500

    added doc strings for valentina

--- test_3dinterpolator_testing.py
+++ test_3dinterpolator_testing.py
@@ -1,85 +1,74 @@
 import numpy as np
 from scipy.interpolate import Rbf
 import pandas as pd
+import mayavi
 from mayavi import mlab
 from scipy.stats import multivariate_normal
 
 
-# generate observations
-left = -2.
+mlab.options.backend = 'envisage'
+
+####### generate fake observations
+
+# generate sampling coordinates
+left = -3.
 right = 3.
 floor = -5
 ceiling = 5
-bottom = -1.
-top = 2.
+downwind = -2.
+upwind = 2.
 
-resolution = 0.7
+ground_tr_resolution = 0.7
 
-x = np.arange(bottom, top, resolution)
-y = np.arange(left, right, resolution)
-z = np.arange(floor, ceiling, resolution)
+x = np.arange(downwind, upwind, ground_tr_resolution)
+y = np.arange(left, right, ground_tr_resolution)
+z = np.arange(floor, ceiling, ground_tr_resolution)
 
 xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
 df_dict = dict()
 df_dict['x'] = xx.ravel()
 df_dict['y'] = yy.ravel()
 df_dict['z'] = zz.ravel()
-
-
 observations = pd.DataFrame(data=df_dict)
 
-
+# generate temperatures with draws from 3d gaussian
 xyz = np.column_stack([xx.flat, yy.flat, zz.flat])
 mu = np.array([0.0, 0.0, 0.0])
-sigma = np.array([1,1,1])
+sigma = np.array([1, 1, 1])
 covariance = np.diag(sigma**2)
-
 temp = multivariate_normal.pdf(xyz, mean=mu, cov=covariance)
-temp = temp.reshape(xx.shape)
+observations['avg_temp'] = temp
+temp_grid = temp.reshape(xx.shape)
 
 
+# make interpolator
+rbfi = Rbf(observations.x.values, observations.y.values, observations.z.values, observations.avg_temp.values,
+           function='gaussian')
 
-# # make some places hot
-#
-# temp = 19.
-# observations['avg_temp'] = np.array([temp] * len(x) * len(y))  # make it the same temperature everywhere
-# # observations.loc[((observations['x'] > -.5) & (observations['x'] < 0.5) & (observations['y'] > -.2) \
-# #         & (observations['y'] < 0.6)), 'avg_temp'] = 50.
-#
-# # slant
-# observations.loc[((observations['x'] > 0) &
-#                   (observations['x'] < top) &
-#                   (observations['y'] > 0) &
-#                   (observations['y'] < right)),
-#                  'avg_temp'] = observations.loc[((observations['x'] > 0) & (observations['x'] < top) & (observations['y'] > 0) & (observations['y'] < right)), 'x'] * 50 +\
-#                     observations.loc[((observations['x'] > 0) & (observations['x'] < top) & (observations['y'] > 0) & (observations['y'] < right)), 'y'] / 50
-# tt = np.reshape(observations.avg_temp, np.shape(xx))
+# define positions to interpolate (at higher resolution than ground truth, above)
+interp_left = -2.
+interp_right = 2.
+interp_floor = -4
+interp_ceiling = 4
+interp_downwind = -1.
+interp_upwind = 1.
 
-### gauss
+interp_bounds = [interp_downwind, interp_upwind, interp_left, interp_right, interp_floor, interp_ceiling]
 
-# observations['avg_temp'] = np.ravel(tt)
+interp_res = ground_tr_resolution/2
 
+xi = np.arange(interp_downwind, interp_upwind, interp_res)
+yi = np.arange(interp_left, interp_right, interp_res)
+zi = np.arange(interp_floor, interp_ceiling, interp_res)
+xxi, yyi, zzi = np.meshgrid(xi, yi, zi, indexing='ij')
+xxi_flat = xxi.ravel()
+yyi_flat = yyi.ravel()
+zzi_flat = zzi.ravel()
 
-#
-#
-# # tt = np.reshape(observations.avg_temp, np.shape(xx))
-#
-# # make interpolator
-# rbfi = Rbf(observations.x.values, observations.y.values, observations.z.values, observations.avg_temp.values,
-#            function='gaussian')
-#
-# # define positions to interpolate at
-# xi = np.linspace(bottom/2, top/2, 10)  # xmin * .8
-# yi = np.linspace(left/2, right/2, 10)
-# zi = np.linspace(floor/2, ceiling/2, 10)
-# xxi, yyi = np.meshgrid(xi, yi, indexing='ij')
-# xxi_flat = xxi.ravel()
-# yyi_flat = yyi.ravel()
-#
-# # interpolate
-# interp_temps = rbfi(xxi_flat, yyi_flat)
-# tti = interp_temps.reshape((len(xi), len(yi)))
-#
+# interpolate
+interp_temps = rbfi(xxi_flat, yyi_flat, zzi_flat)
+tti = interp_temps.reshape((len(xi), len(yi), len(zi)))
+
 # print """
 #         Interpolated temp stats
 #         min {}
@@ -89,18 +78,33 @@ temp = temp.reshape(xx.shape)
 #
 
 # Plot scatter with mayavi
-figure = mlab.figure('DensityPlot', bgcolor=(1, 1, 1))
+"""
+stackexchange question for cmaps: https://stackoverflow.com/questions/36946231/using-perceptually-uniform-colormaps-in-mayavi-volumetric-visualization
+bug report for custom colormaps: https://github.com/enthought/mayavi/issues/371
 
 
-
-# grid = mlab.pipeline.scalar_field(xi, yi, zi, density)
-# min = density.min()
-# max=density.max()
-# s = mlab.pipeline.volume(grid, vmin=min, vmax=max,) #min + .5*(max-min))
+"""
 
 
-src = mlab.pipeline.scalar_field(temp)
+## visualize
+gr_truth_fig = mlab.figure('Ground truth observations')
+
+
+src = mlab.pipeline.scalar_field(temp_grid)
 vol = mlab.pipeline.volume(src, vmin=temp.min(), vmax=temp.max()*.7)
+mayavi.tools.pipeline.set_extent(mlab.pipeline.volume, interp_bounds)
 
+
+# mlab.show()
+
+###################
+interp_fig = mlab.figure('Interpolation')
+
+src = mlab.pipeline.scalar_field(tti)
+vol = mlab.pipeline.volume(src, vmin=tti.min(), vmax=tti.max()*.7)
+
+# mlab.axes(bounds=interp_bounds)
+
+#########
 mlab.axes()
 mlab.show()
