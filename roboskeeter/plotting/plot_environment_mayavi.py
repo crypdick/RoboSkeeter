069b1964544bf801887ac621945264f46d62ffd1
Richard Decal <decal@uw.edu>
2016-05-12 19:05:38 -0500

    3d quiver plots

--- roboskeeter/plotting/plot_environment_mayavi.py
+++ roboskeeter/plotting/plot_environment_mayavi.py
@@ -15,3 +15,13 @@ def plot_plume_recordings_volume(bounds, grid_x, grid_y, grid_z, grid_temps):
 
     mlab.axes(bounds=bounds)
     mlab.show()
+
+def plot_plume_3d_quiver(u, v, w):
+    src = mlab.pipeline.vector_field(u, v, w)
+    mlab.pipeline.vectors(src, mask_points=200, scale_factor=2.)
+    mlab.show()
+    #
+    # flow = mlab.flow(u, v, w, seed_scale=1,
+    #                       seed_resolution=5,
+    #                       integration_direction='both')
+    # mlab.show()
