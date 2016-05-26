190a11553b0fd48f64d189fbe8870b8e28b3eb77
Richard Decal <decal@uw.edu>
2016-05-26 16:44:38 -0500

    replaced dkl with 2 sample ks test

--- roboskeeter/math/scoring/scoring.py
+++ roboskeeter/math/scoring/scoring.py
@@ -1,13 +1,11 @@
 __author__ = 'richard'
 
 import numpy as np
-from scipy.stats import entropy, ks_2samp
-from roboskeeter.math.math_toolbox import calculate_1Dkde, evaluate_kde
+from scipy.stats import ks_2samp
 
 
 
 class Scoring():
-    # TODO: when called from an experiment class, find out the relevant experiment from metadata, load that experiment, use as reference
     def __init__(self,
                  target_experiment,
                  reference_experiment=None,
@@ -24,96 +22,66 @@ class Scoring():
                                 'curvature': 3}
                  ):
         if reference_experiment is None:
-            raise NotImplementedError
-        else:
-            self.reference = reference_experiment
-        self.target = target_experiment
-        self.score = None
-        self.target_histograms = None
-        self.DKLs = None
+            # when called from an experiment class, find out the relevant experiment from metadata, load that experiment, use as reference
+            reference_experiment = self.load_reference_ensemble(target_experiment.experiment_conditions['condition'])
 
-    def score(self, ensemble, kinematics_list=['velocities', 'curvature']):
-        self.targ_vals = self._get_target_data(ensemble)
-        self.dkls = self._calc_dkls(kinematics_list)
-        self.dkl_sum = sum(self.dkls)
+        self.score_weights = score_weights
 
-        return self.dkl_sum
+        self.target_data, self.reference_data = self._select_data(target_experiment, reference_experiment)
 
-    def _select_data(self):
+        self.score, self.score_components = self.calc_score()
+
+    def calc_score(self):
+        """
+        Solves KS-2 sample test for test and reference data, weighted by the score weights kwarg
+
+        Returns
+        -------
+        total score and score components
+        """
+        score_components = dict()
+        for k, v in self.target_data.iteritems():
+            ks, pval = ks_2samp(v, self.reference_data[k])
+            score_components[k] = self.score_weights[k] * ks
+
+        return sum(score_components.values()), score_components
+
+    def load_reference_ensemble(self, condition):
+        from roboskeeter import experiments
+        reference_experiment = experiments.load_experiment(experiment_conditions = {'condition': condition,
+                                 'plume_model': "None",  # "Boolean" "None, "Timeavg", "Unaveraged"
+                                 'time_max': "N/A (experiment)",
+                                 'bounded': True,
+                                 })
+        return reference_experiment
+
+    def _select_data(self, target_experiment, reference_experiment):
         # TODO: make depend on self.scoring_kwargs
-        data = {'v_x': trajectory.data['velocity_x'].values,
-                'v_y': trajectory.data['velocity_y'].values,
-                'v_z': trajectory.data['velocity_z'].values,
-    #            'a_x': np.abs(trajectory.data['acceleration_x'].values),
-    #            'a_y': np.abs(trajectory.data['acceleration_y'].values),
-    #            'a_z': np.abs(trajectory.data['acceleration_z'].values),
-                'c': trajectory.data['curvature'].values
-                }
-        return data
+        r_k = reference_experiment.observations.kinematics
+        t_k = target_experiment.observations.kinematics
 
 
-    def _evaluate_histograms(self):
-        self._pick_bins()
-        targ_data = self._select_data()
-        targ_KDEs = calc_kde(targ_data)
-        targ_vals = evaluate_kdes(targ_KDEs,
-                                  self.experimental_bins)  # we evaluate targ KDE at experimental bins for comparison
+        reference_data = {'velocity_x': r_k['velocity_x'].values,
+                          'velocity_y': r_k['velocity_y'].values,
+                          'velocity_z': r_k['velocity_z'].values,
+                          'position_x': r_k['position_x'].values,
+                          'position_y': r_k['position_y'].values,
+                          'position_z': r_k['position_z'].values,
+                          'acceleration_x': np.abs(r_k['acceleration_x'].values),
+                          'acceleration_y': np.abs(r_k['acceleration_y'].values),
+                          'acceleration_z': np.abs(r_k['acceleration_z'].values),
+                          'curvature': r_k['curvature'].values
+                          }
 
-        return targ_vals
-
-    def _pick_bins(self):
-        pad_coeff = 2.  # pad the distribution to properly penalize values above
-        bins_dict = {}
-        for k, vect in data.items():
-            lower, upper = vect.min(), vect.max()
-            if lower > 0:
-                lower = 0.
-            bins_dict[k] = np.linspace(lower * pad_coeff, upper * pad_coeff, 2000)
-
-    def _calc_dkls(self, kinematics_list):
-        kinematic_dict = {'velocities': ['v_x', 'v_y', 'v_z'],
-                          'accelerations': ['a_x', 'a_y', 'a_z'],
-                          'curvature': ['c'],
-                          'positions': ['p_x', 'p_y', 'p_z'],
-                          'crosswind_position': ['p_y'],
-                          'crosswind_velocity': ['v_y']}
-
-        kinematic_tokens = []
-        for kin in kinematics_list:
-            kinematic_tokens.extend(kinematic_dict[kin])
-
-        dkls = []
-        for token in kinematic_tokens:
-            exp_distribution = self.experimental_vals[token]
-            targ_distribution = self.targ_vals[token]
-            dkls.append(entropy(targ_distribution, qk=exp_distribution))
-
-        return dkls
-
-
-def calc_bins(data):
-
-
-
-    return bins_dict
-
-
-def calc_kde(data):
-    '''takes a dictionary of vectors and returns a kernel density function of each vector '''
-    kdes_dict = {}
-
-    for k, v in data.items():
-        kdes_dict[k] = calculate_1Dkde(v)
-
-    return kdes_dict
-
-
-def evaluate_kdes(kdes_dict, bins_dict):
-    '''given a KDE function and positions, it will evaluate the KDE function at those locations '''
-    vals_dict = {}
-
-    for kinem, kde in kdes_dict.items():
-        bins = bins_dict[kinem]
-        vals_dict[kinem] = evaluate_kde(kde, bins)
-
-    return vals_dict
+        target_data =    {'velocity_x': t_k['velocity_x'].values,
+                          'velocity_y': t_k['velocity_y'].values,
+                          'velocity_z': t_k['velocity_z'].values,
+                          'position_x': t_k['position_x'].values,
+                          'position_y': t_k['position_y'].values,
+                          'position_z': t_k['position_z'].values,
+                          'acceleration_x': np.abs(t_k['acceleration_x'].values),
+                          'acceleration_y': np.abs(t_k['acceleration_y'].values),
+                          'acceleration_z': np.abs(t_k['acceleration_z'].values),
+                          'curvature': t_k['curvature'].values
+                          }
+        return target_data, reference_data
